9d2859b60b5efc1d240ea7f0793541b3db238ed8
khadija <khadijamahanga@y7mail.com>
2020-03-18 11:52:39 +0300

    introduce transform imports

--- src/components/ChartsContainer.js
+++ src/components/ChartsContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import { PropTypes } from 'prop-types';
-import { makeStyles, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   root: {
