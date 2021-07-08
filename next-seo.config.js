1f712a5008289497a520c29b7bb502a563661d1c
nerdlet <nyokabi@codeforafrica.org>
2021-07-08 17:36:02 +0300

    Minor update

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,18 +1,17 @@
 export default {
- titleTemplate: "%s | PesaYetu",
- defaultTitle: "PesaYetu",
- description:
-   "County data to hold your government accountable",
- openGraph: {
-   type: "website",
-   url: "https://dev.pesayetu.pesacheck.org",
-   locale: "en_TZ",
-   site_name: "PesaYetu",
- },
- additionalMetaTags: [
-   {
-     name: "viewport",
-     content: "minimum-scale=1, initial-scale=1, width=device-width",
-   },
- ],
+  titleTemplate: "%s | PesaYetu",
+  defaultTitle: "PesaYetu",
+  description: "County data to hold your government accountable",
+  openGraph: {
+    type: "website",
+    url: "https://dev.pesayetu.pesacheck.org",
+    locale: "en_TZ",
+    site_name: "PesaYetu",
+  },
+  additionalMetaTags: [
+    {
+      name: "viewport",
+      content: "minimum-scale=1, initial-scale=1, width=device-width",
+    },
+  ],
 };
--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+ enabled: process.env.ANALYZE === "true",
+});
 const withImages = require("next-images");
-module.exports = withImages({
+
+module.exports = withBundleAnalyzer(
+ withImages({
    cssLoaderOptions: {
      url: false,
    },
    // Exclude SVG from default file extensions list
    // See: https://github.com/twopluszero/next-images/blob/master/index.js
    fileExtensions: ["jpg", "jpeg", "png", "gif", "ico", "webp", "jp2", "avif"],
+   images: {
+     domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS.split(", "),
+   },
    webpack(config) {
      config.module.rules.push({
        test: /\.svg$/,
@@ -19,4 +27,5 @@ module.exports = withImages({
      });
      return config;
    },
- });
+ })
+);
