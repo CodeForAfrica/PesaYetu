3477db65ab14b65693991eb3c13646447a440fea
nerdlet <nyokabi@codeforafrica.org>
2021-07-08 18:18:42 +0300

    Update files for deployment

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,12 +1,22 @@
 export default {
   titleTemplate: "%s | PesaYetu",
   defaultTitle: "PesaYetu",
-  description: "County data to hold your government accountable",
+  description:
+    "County data to hold your government accountable",
   openGraph: {
     type: "website",
     url: "https://dev.pesayetu.pesacheck.org",
     locale: "en_TZ",
     site_name: "PesaYetu",
+    images: [
+      {
+        /* eslint-disable global-require */
+        url: "",
+        width: 994,
+        height: 511,
+        alt: "pesayetu",
+      },
+    ],
   },
   additionalMetaTags: [
     {
