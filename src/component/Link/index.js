fefe4f0958aafd403d93a554a205e167bce63631
nerdlet <nyokabi@codeforafrica.org>
2021-07-08 21:18:51 +0300

    Update lint

--- src/component/Link/index.js
+++ src/component/Link/index.js
@@ -97,6 +97,7 @@ Link.defaultProps = {
   prefetch: undefined,
 };
 
+// eslint-disable-next-line react/display-name
 export default React.forwardRef((props, ref) => (
   <Link {...props} innerRef={ref} />
 ));
