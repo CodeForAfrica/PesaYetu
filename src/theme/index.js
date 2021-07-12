caccea2bedf7cbbf6fbe15f2e450b85c50493012
nerdlet <nyokabi@codeforafrica.org>
2021-07-12 18:01:59 +0300

    Add theme fonts

--- src/theme/index.js
+++ src/theme/index.js
@@ -13,29 +13,47 @@ const theme = createTheme({
   },
   palette: {
     primary: {
-      dark: '#0E68A1', // blue
-      light: '#7DB2D3',
-      main: '#0067A3',
+      main: '#0067A3', // main
+      light: '#7DB2D3', // hightlight blue
     },
     secondary: {
-      main: '#EE4538', // red
-      light: '#F8A199',
+      main: '#EE4538', // main red
+      light: '#F8A199', // highlight red
     },
     background: {
-      main: '#FFFFFF',
-      light: '#F8F8F8', // white
-      dark: '#2A2A2C',
-      mediumgrey: '#DFDFDF',
+      main: '#FFF', // white
+      light: '#F8F8F8',
+      dark: '#2A2A2C ', // dark black
+      darkgrey: '#DFDFDF',
       lightgrey: '#F0F0F0',
     },
     text: {
       main: '#333333',
-      dark: '#212529',
+      subtitle: '#212529',
       headings: '#1C2031',
     },
   },
   typography: {
     fontFamily: FONT_FAMILY_TEXT,
+    h1: {
+      fontWeight: 'black',
+    },
+    h2: {
+      fontWeight: 'light',
+    },
+    h3: {
+      fontWeight: 'semi-bold',
+    },
+    h4: {
+      fontWeight: 'medium',
+    },
+    h5: {
+      fontWeight: 'bold',
+    },
+    body1: {},
+    body2: {},
+    subtitle1: {},
+    subtitle2: {},
   },
   widths: {
     values: {
@@ -45,22 +63,92 @@ const theme = createTheme({
   },
 });
 
-const { palette, typography, overrides } = theme;
+const { palette, typography, breakpoints, overrides } = theme;
 const { pxToRem } = typography;
 // Typography
 deepmerge(
   typography,
   {
-    h1: {},
-    h2: {},
-    h3: {},
-    h4: {},
-    h5: {},
-    body1: {},
-    body2: {},
-    caption: {},
-    subtitle1: {},
-    subtitle2: {},
+    h1: {
+      fontSize: pxToRem(50),
+      lineHeight: 66 / 50,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(50),
+        lineHeight: 66 / 50, // font 50 H0
+      },
+    },
+    h2: {
+      fontSize: pxToRem(38),
+      lineHeight: 48 / 38,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(48),
+        lineHeight: 58 / 48, // font 48 H1
+      },
+    },
+    h3: {
+      fontSize: pxToRem(24),
+      lineHeight: 40 / 24,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(30),
+        lineHeight: 48 / 30, // font 30 H2
+      },
+    },
+    h4: {
+      fontSize: pxToRem(20),
+      lineHeight: 30 / 20,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(20),
+        lineHeight: 30 / 20, // font 20 H4
+      },
+    },
+    h5: {
+      fontSize: pxToRem(18),
+      lineHeight: 27 / 18,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(18),
+        lineHeight: 27 / 18, // font 18 H5
+      },
+    },
+    body1: {
+      fontSize: pxToRem(16),
+      lineHeight: 30 / 16,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(16),
+        lineHeight: 30 / 16, // font 18 body1
+      },
+    },
+    body2: {
+      fontSize: pxToRem(16),
+      lineHeight: 24 / 16,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(16),
+        lineHeight: 24 / 16, // font body2
+      },
+    },
+    subtitle1: {
+      fontSize: pxToRem(14),
+      lineHeight: 24 / 14,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(14),
+        lineHeight: 24 / 14, // font 14 subtitle1
+      },
+    },
+    subtitle2: {
+      fontSize: pxToRem(14),
+      lineHeight: 18 / 14,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(14),
+        lineHeight: 18 / 14, // font 14 subtitle2
+      },
+    },
+    caption: {
+      fontSize: pxToRem(12),
+      lineHeight: 18 / 12,
+      [breakpoints.up('lg')]: {
+        fontSize: pxToRem(12),
+        lineHeight: 18 / 12, // font 12 caption
+      },
+    },
   },
   { clone: false }
 );
