f67a6f476f544c3b89b95973e32b4e2ecd0061c6
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-07-13 23:31:39 +0300

    hardcoded data

--- src/component/ExploreCard/index.js
+++ src/component/ExploreCard/index.js
@@ -1,27 +1,35 @@
 import { Card, CardMedia, Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import useStyles from './useStyles';
 
-import GroupImage from '@/pesayetu/assets/Group 3973.png';
-
-const ExploreCard = (props) => {
+const ExploreCard = ({ item, ...props }) => {
   const classes = useStyles(props);
+  const { title, description, image } = item;
   return (
     <div className={classes.root}>
       <Card className={classes.card}>
-        <CardMedia className={classes.image} image={GroupImage} />
+        <CardMedia className={classes.image} image={image} />
       </Card>
       <Typography className={classes.title} variant="h4">
-        Promise Tracker
+        {title}
       </Typography>
-      <Typography className={classes.description}>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit, magnis mus etiam
-        nam lectus lobortis varius ultrices, donec dapibus dui felis est
-        penatibus.
-      </Typography>
+      <Typography className={classes.description}>{description}</Typography>
     </div>
   );
+};
+
+ExploreCard.propTypes = {
+  item: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string,
+  }),
+};
+
+ExploreCard.defaultProps = {
+  item: undefined,
 };
 
 export default ExploreCard;
