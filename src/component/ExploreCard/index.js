dc3e8cb6122f52f903c53da679a4aafc4c4f575d
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-07-13 11:14:51 +0300

    add card style file + image

--- src/component/ExploreCard/index.js
+++ src/component/ExploreCard/index.js
@@ -1,5 +1,19 @@
+import { Card, CardMedia } from '@material-ui/core';
 import React from 'react';
 
-const ExploreCard = () => <div>Explore Card</div>;
+import useStyles from './useStyles';
+
+import GroupImage from '@/pesayetu/assets/Group 3973.png';
+
+const ExploreCard = (props) => {
+  const classes = useStyles(props);
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardMedia image={GroupImage} />
+      </Card>
+    </div>
+  );
+};
 
 export default ExploreCard;
