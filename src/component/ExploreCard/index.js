fdf93267314ca0f52d6c524487c56c959bca408a
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-07-13 15:57:25 +0300

    styling

--- src/component/ExploreCard/index.js
+++ src/component/ExploreCard/index.js
@@ -1,4 +1,4 @@
-import { Card, CardMedia } from '@material-ui/core';
+import { Card, CardMedia, Typography } from '@material-ui/core';
 import React from 'react';
 
 import useStyles from './useStyles';
@@ -8,10 +8,18 @@ import GroupImage from '@/pesayetu/assets/Group 3973.png';
 const ExploreCard = (props) => {
   const classes = useStyles(props);
   return (
-    <div>
-      <Card className={classes.root}>
-        <CardMedia image={GroupImage} />
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <CardMedia className={classes.image} image={GroupImage} />
       </Card>
+      <Typography className={classes.title} variant="h4">
+        Promise Tracker
+      </Typography>
+      <Typography className={classes.description}>
+        Lorem ipsum dolor sit amet consectetur adipiscing elit, magnis mus etiam
+        nam lectus lobortis varius ultrices, donec dapibus dui felis est
+        penatibus.
+      </Typography>
     </div>
   );
 };
--- src/component/ExploreCard/useStyles.js
+++ src/component/ExploreCard/useStyles.js
@@ -1,7 +1,25 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
-  root: {},
+const useStyles = makeStyles(({ breakpoints, typography }) => ({
+  root: {
+    width: typography.pxToRem(350),
+    [breakpoints.up('md')]: {
+      width: typography.pxToRem(296),
+    },
+    [breakpoints.up('lg')]: {
+      width: typography.pxToRem(356),
+    },
+  },
+  card: {},
+  image: {
+    height: typography.pxToRem(220),
+    backgroundSize: 'auto',
+  },
+  title: {
+    marginTop: typography.pxToRem(20),
+    marginBottom: typography.pxToRem(20),
+  },
+  description: {},
 }));
 
 export default useStyles;
