a42f99b7677e3b3d7163e95c4979118d3bbceece
khadija <khadijamahanga@y7mail.com>
2021-07-14 07:36:01 +0300

    remove console

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,24 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import getPostTypeStaticProps from '@/pesayetu/functions/postTypes/getPostTypeStaticProps';
 
-export default function Home() {
+export default function Home({ ...props }) {
+  if (props?.errorMessage) {
+    return <div> {props.errorMessage}</div>;
+  }
   return <div>Pesa yetu homepage template</div>;
 }
 
+Home.propTypes = {
+  errorMessage: PropTypes.string,
+};
+
+Home.defaultProps = {
+  errorMessage: undefined,
+};
+
 export async function getStaticProps() {
   const postType = 'page';
-  const { props } = await getPostTypeStaticProps({ slug: '/' }, postType);
-
-  console.log(props);
-
-  return {
-    props,
-  };
+  return getPostTypeStaticProps({ slug: '/' }, postType);
 }
