2ea6cca350ca4ac13992362f872998f146afd603
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-07-16 14:18:02 +0300

    add section

--- src/component/DataIndicators/index.js
+++ src/component/DataIndicators/index.js
@@ -9,20 +9,23 @@ import Group4656 from '@/pesayetu/assets/Group 4656.png';
 import Group4657 from '@/pesayetu/assets/Group 4657.png';
 import Group4658 from '@/pesayetu/assets/Group 4658.png';
 import Group4659 from '@/pesayetu/assets/Group 4659.png';
+import Section from '@/pesayetu/component/Section';
 
 const DataIndicators = (props) => {
   const classes = useStyles(props);
 
   return (
     <div className={classes.root}>
-      <Typography>Data Indicators</Typography>
-      <Grid container>
-        <Image src={Group4657} />
-        <Image src={Group4656} />
-        <Image src={Group4646} />
-        <Image src={Group4659} />
-        <Image src={Group4658} />
-      </Grid>
+      <Section classes={{ root: classes.section }}>
+        <Typography className={classes.title}>Data Indicators</Typography>
+        <Grid container>
+          <Image src={Group4657} />
+          <Image src={Group4656} />
+          <Image src={Group4646} />
+          <Image src={Group4659} />
+          <Image src={Group4658} />
+        </Grid>
+      </Section>
     </div>
   );
 };
--- src/component/DataIndicators/useStyles.js
+++ src/component/DataIndicators/useStyles.js
@@ -1,7 +1,19 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
-  root: {},
+const useStyles = makeStyles(({ typography }) => ({
+  root: {
+    backgroundColor: '#F0F0F0',
+  },
+  title: {
+    textAlign: 'center',
+    paddingBottom: typography.pxToRem(80),
+    fontSize: typography.pxToRem(48),
+    fontWeight: 900,
+  },
+  section: {
+    paddingTop: typography.pxToRem(102),
+    paddingBottom: typography.pxToRem(163),
+  },
 }));
 
 export default useStyles;
