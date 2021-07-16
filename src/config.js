afe0c523b4bf4eab633c04a73389888c36ba243d
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-07-16 07:57:29 +0300

    add data to config

---
+++ src/config.js
@@ -0,0 +1,32 @@
+const dataIndicator = {
+  title: 'Data Indicators',
+  items: [
+    {
+      title: 'Overview',
+      description:
+        'This includes general county data. Topics include administrative and political units, population size and composition, land use, tourism and wildlife, industry and trade, finance, and education.',
+    },
+    {
+      title: 'Revenue',
+      description:
+        'This looks at the review of the implementation of the previous County Integrated Development Plan for the period of 2014 to 2017. Datasets include county revenue streams and expenditure analysis.',
+    },
+    {
+      title: 'Development',
+      description:
+        'This highlights the county development priorities and strategies. It looks at the programmes and associated budgets the county has proposed for the period 2018 to 2022.',
+    },
+    {
+      title: 'Implement',
+      description:
+        'This showcases the implementation framework for the county as published in the County Integrated Development Plan. It covers the resource mobilisation framework on the proposed and predicted revenue and expenditure.',
+    },
+    {
+      title: 'Summary',
+      description:
+        'The summary takes a look at the monitoring and evaluation framework as published in the County Integrated Development Plan. This includes the outcome indicators for each of the sector plans proposed.',
+    },
+  ],
+};
+
+export default dataIndicator;
