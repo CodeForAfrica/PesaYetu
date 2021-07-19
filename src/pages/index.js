8d0fa66ce215640aeff792a2067f472669db1b14
nerdlet <nyokabi@codeforafrica.org>
2021-07-19 10:59:25 +0300

    Add page to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Page from '@/pesayetu/component/Page';
 import getPostTypeStaticProps from '@/pesayetu/functions/postTypes/getPostTypeStaticProps';
 
 export default function Home({ ...props }) {
   if (props?.errorMessage) {
     return <div> {props.errorMessage}</div>;
   }
-  return <div>Pesa yetu homepage template</div>;
+  return <Page>Pesa yetu homepage template</Page>;
 }
 
 Home.propTypes = {
