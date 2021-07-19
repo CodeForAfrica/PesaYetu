277b06fb34ecac36e8e834c5e686f6f43043bb1c
khadija <khadijamahanga@y7mail.com>
2021-07-20 00:36:51 +0300

    rename component to components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import Page from '@/pesayetu/component/Page';
+import Page from '@/pesayetu/components/Page';
 import getPostTypeStaticProps from '@/pesayetu/functions/postTypes/getPostTypeStaticProps';
 
 export default function Home({ ...props }) {
