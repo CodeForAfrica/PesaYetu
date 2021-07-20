d4328c80fad96fc3b7e9ceccdca148ffe1eb1216
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-07-21 00:49:34 +0300

    remove Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import ExploreSection from '@/pesayetu/component/ExploreSection';
-import Page from '@/pesayetu/component/Page';
 import { exploreTools } from '@/pesayetu/config';
 
 export default function Home() {
   return (
-    <Page>
+    <>
       <ExploreSection {...exploreTools} />
-    </Page>
+    </>
   );
 }
