64d7a41d0dd4dbd733af72863a2c8afda50aae55
nerdlet <nyokabi@codeforafrica.org>
2021-07-21 12:18:48 +0300

    Add menu

--- src/component/MenuNavigation/index.js
+++ src/component/MenuNavigation/index.js
@@ -1,7 +1,71 @@
+import { Grid, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 
-function MenuNavigation() {
-  return <div>this is the menu</div>;
+const useStyles = makeStyles(({ typography }) => ({
+  root: {
+    padding: `${typography.pxToRem(9.25)} 0`,
+  },
+}));
+
+const menuButtons = [
+  {
+    href: 'https://dev.pesayetu.pesacheck.org',
+    label: 'EXPLORE',
+    menuLinks: [
+      {
+        href: 'https://dev.pesayetu.pesacheck.org',
+        label: 'DATA',
+      },
+      {
+        href: 'https://dev.pesayetu.pesacheck.org',
+        label: 'STORIES',
+      },
+      {
+        href: 'https://dev.pesayetu.pesacheck.org',
+        label: 'HOW IT WORKS',
+      },
+    ],
+  },
+];
+const allMenuLinks = menuButtons.map((item) => item.menuLinks);
+// eslint-disable-next-line no-console
+console.log(allMenuLinks[0]);
+
+function MenuNavigation({ ...props }) {
+  const classes = useStyles(props);
+  return (
+    <Grid
+      container
+      item
+      xs={8}
+      direction="row"
+      justify="flex-start"
+      alignItem="center"
+      className={classes.root}
+    >
+      {menuButtons.map((item) => (
+        <Grid item>
+          <Button
+            color="primary"
+            variant="contained"
+            size="large"
+            href={item.href}
+          >
+            {item.label}
+          </Button>
+        </Grid>
+      ))}
+
+      {allMenuLinks[0].map((item) => (
+        <Grid item>
+          <Button color="default" variant="text" size="large" href={item.href}>
+            {item.label}
+          </Button>
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
 export default MenuNavigation;
