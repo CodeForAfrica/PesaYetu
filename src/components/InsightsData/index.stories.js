e5336317fc82512c20f94ee87aa69ceabd64addc
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-07-23 02:21:52 +0300

    add to storybook

--- src/components/InsightsData/index.stories.js
+++ src/components/InsightsData/index.stories.js
@@ -2,16 +2,15 @@
 import React from "react";
 
 import InsightsData from "@/pesayetu/components/InsightsData";
+import { insightData } from "@/pesayetu/config";
 
 export default {
   title: "Sections/InsightsData",
   argTypes: {},
 };
 
-const Template = () => <InsightsData />;
+const Template = ({ ...args }) => <InsightsData {...args} />;
 
 export const Default = Template.bind({});
 
-Default.args = {
-  item: null,
-};
+Default.args = insightData;
