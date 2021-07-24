94d4eea2f8aee1a7ceaeddb91d6f009017e7f7fd
nerdlet <nyokabi@codeforafrica.org>
2021-07-24 22:37:32 +0300

    Minor update

--- src/components/Menu/index.stories.js
+++ src/components/Menu/index.stories.js
@@ -3,9 +3,6 @@ import { withNextRouter } from 'storybook-addon-next-router';
 
 import Menu from '.';
 
-import { ReactComponent as Facebook } from '@/pesayetu/assets/footer-social-fb.svg';
-import { ReactComponent as Twitter } from '@/pesayetu/assets/footer-social-tw.svg';
-
 const menuButtons = [
   {
     href: 'https://dev.pesayetu.pesacheck.org',
@@ -24,18 +21,6 @@ const menuButtons = [
         label: 'HOW IT WORKS',
       },
     ],
-  },
-];
-const socialLinks = [
-  {
-    href: '/twitter',
-    component: Twitter,
-    label: 'twitter',
-  },
-  {
-    href: '/facebook',
-    component: Facebook,
-    label: 'facebook',
   },
 ];
 
@@ -62,5 +47,4 @@ export const Default = Template.bind({});
 
 Default.args = {
   links: menuButtons,
-  socialLinks,
 };
