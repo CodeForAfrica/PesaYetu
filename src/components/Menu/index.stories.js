8956687aa366c932a8a1cc1e67b8f9a25f6a4859
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2021-07-27 11:14:02 +0300

    Update menu

--- src/components/Menu/index.stories.js
+++ src/components/Menu/index.stories.js
@@ -2,8 +2,7 @@ import React from "react";
 
 import Menu from ".";
 
-import { ReactComponent as Facebook } from "@/pesayetu/assets/footer-social-fb.svg";
-import { ReactComponent as Twitter } from "@/pesayetu/assets/footer-social-tw.svg";
+import { navigationArgs } from "@/pesayetu/config";
 
 export default {
   title: "Components/Menu",
@@ -26,36 +25,6 @@ const Template = ({ ...args }) => <Menu {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  links: [
-    {
-      href: "https://dev.pesayetu.pesacheck.org",
-      label: "EXPLORE",
-      menuLinks: [
-        {
-          href: "https://dev.pesayetu.pesacheck.org",
-          label: "DATA",
-        },
-        {
-          href: "https://dev.pesayetu.pesacheck.org",
-          label: "STORIES",
-        },
-        {
-          href: "https://dev.pesayetu.pesacheck.org",
-          label: "HOW IT WORKS",
-        },
-      ],
-    },
-  ],
-  socialLinks: [
-    {
-      href: "/twitter",
-      component: Twitter,
-      label: "twitter",
-    },
-    {
-      href: "/facebook",
-      component: Facebook,
-      label: "facebook",
-    },
-  ],
+  links: navigationArgs.menuProps,
+  socialLinks: navigationArgs.socialLinks,
 };
