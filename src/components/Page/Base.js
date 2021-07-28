8f7e380567ee4a0b4cfaf0a56bd837a69dd4e7d0
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2021-07-28 13:56:32 +0300

    Organize footerMenu

--- src/components/Page/Base.js
+++ src/components/Page/Base.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { NextSeo } from "next-seo";
 import PropTypes from "prop-types";
 import React from "react";
@@ -8,12 +9,84 @@ import { footerArgs } from "@/pesayetu/config";
  * Base page that can be used to build all other pages.
  */
 
-function BasePage({ children, ...props }) {
+function BasePage({ children, menus, ...props }) {
+  const footerProps = {
+    ...footerArgs,
+    links: menus?.footerMenu ?? [],
+  };
+  const getFooterMenu = (data) => {
+    const logosProps = data
+      .filter((item) => item.label.toLowerCase() === "logo")
+      .map(({ label, url, title, description }) => {
+        const CFA = {
+          image: {
+            src: url,
+            alt: label.toLowerCase(),
+          },
+          url: title,
+          description,
+        };
+        return {
+          logoProps: CFA,
+        };
+      });
+    const socialLinks = data
+      .filter((item) => item.label.toLowerCase() === "stay in touch")
+      // eslint-disable-next-line no-shadow
+      .map(({ children }) => {
+        const socialLinksProps = children.map(({ label, url, title }) => {
+          return {
+            image: {
+              src: url,
+              alt: label.toLowerCase(),
+            },
+            url: title,
+            label,
+          };
+        });
+        return {
+          socialLinksProps,
+        };
+      });
+    const copyrightProps = data
+      .filter((item) => item.label.toLowerCase() === "copyright")
+      // eslint-disable-next-line no-shadow
+      .map(({ url, description, title }) => {
+        const copyright = {
+          icon: url,
+          copyright: description,
+          copyrightUrl: title,
+        };
+        return {
+          copyrightProps: copyright,
+        };
+      });
+    const quickLinks = data
+      .filter((item) => item.label.toLowerCase() === "resources")
+      // eslint-disable-next-line no-shadow
+      .map(({ label, children }) => {
+        const quickLinksProps = {
+          title: label,
+          // eslint-disable-next-line no-shadow
+          links: children.map(({ label, url }) => {
+            return {
+              label,
+              url,
+            };
+          }),
+        };
+        return {
+          quickLinksProps,
+        };
+      });
+    return { logosProps, socialLinks, copyrightProps, quickLinks };
+  };
+  getFooterMenu(menus?.footerMenu ?? []);
   return (
     <>
       <NextSeo {...props} />
       {children}
-      <Footer {...footerArgs} />
+      <Footer {...footerProps} />
     </>
   );
 }
@@ -23,10 +96,14 @@ BasePage.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]),
+  menus: PropTypes.shape({
+    footerMenu: PropTypes.arrayOf(PropTypes.shape({})),
+  }),
 };
 
 BasePage.defaultProps = {
   children: undefined,
+  menus: undefined,
 };
 
 export default BasePage;
--- src/functions/menus/filterMenusByLocation.js
+++ src/functions/menus/filterMenusByLocation.js
@@ -1,28 +1,27 @@
-import formatHeirarchialMenu from "./formatHeirarchialMenu";
+import { camelCase } from "lodash";
 
+import formatHierarchicalMenu from "@/pesayetu/functions/menus/formatHeirarchialMenu";
 /**
  * Filter menus array by menu location.
  *
- * @param  {Array}  menus     The array of WP menus to filter.
- * @param  {Array}  locations The array of locations for filtering.
- * @return {object}           An object containing the requested locations as individual objects.
+ * @author WebDevStudios
+ * @param {Array} menus     The array of WP menus to filter.
+ * @param {Array} locations The array of locations for filtering.
+ * @return {object}         An object containing the requested locations as individual objects.
  */
 export default function filterMenusByLocation(menus, locations) {
   const data = {};
-
   // Loop each menu location.
   locations.forEach((location) => {
     // Convert dashes to underscores.
     const locationName = location.replace(/-/g, "_");
-
     // Filter menus array by location and assign to new object.
-    const wpmenu = menus.filter((menu) => {
+    const wpmenu = menus?.filter(function filterMenu(menu) {
       return menu.locations.includes(locationName.toUpperCase());
     });
-
+    const camelLocation = camelCase(locationName);
     // Format the returned menu.
-    data[locationName] = formatHeirarchialMenu(wpmenu[0]?.menuItems?.nodes);
+    data[camelLocation] = formatHierarchicalMenu(wpmenu[0]?.menuItems?.nodes);
   });
-
   return data;
 }
