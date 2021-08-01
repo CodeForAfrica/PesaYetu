1348c9413dacbaece2c40061776d50efdddd69e3
Obed Amoasi <obedamoasi@gmail.com>
2021-08-01 21:27:32 +0000

    [wip] style nestletter section

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -1,20 +1,76 @@
-import { RichTypography, Section } from "@commons-ui/core";
+import { RichTypography } from "@commons-ui/core";
 import { Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import React from "react";
 
-const useStyles = makeStyles(({ breakpoints, typography }) => ({
+import email from "@/pesayetu/assets/icons/Component 117 – 1@2x.png";
+
+const useStyles = makeStyles(({ typography, palette }) => ({
   section: {},
+  title: {
+    fontWeight: "900",
+    color: palette.grey.dark,
+  },
+  titleContainer: {},
   root: {
-    alignItems: "center",
-    backgroundColor: "#90DAFF",
     display: "flex",
-    justifyContent: "center",
-    padding: `${typography.pxToRem(36)} 0 ${typography.pxToRem(49)}`,
     width: "100%",
-    [breakpoints.up("lg")]: {
-      padding: `${typography.pxToRem(25)} 0`,
+    minHeight: typography.pxToRem(400),
+    paddingTop: typography.pxToRem(108.09),
+    paddingLeft: typography.pxToRem(116),
+  },
+  form: {
+    "& #mc_embed_signup": {
+      background: "none !important",
+      color: "inherit",
+    },
+    "& #mc_embed_signup form": {
+      padding: 0,
+    },
+    "& #mc_embed_signup label": {
+      display: "none",
+    },
+    "& #mc_embed_signup_scroll": {
+      display: "flex",
+      "&>div:first-of-type": {
+        display: "none !important",
+      },
+    },
+    "& #mc_embed_signup input.email": {
+      background: "none",
+      border: "none",
+      borderBottom: "1px solid currentColor",
+      borderRadius: 0,
+      color: "currentColor",
+      margin: "1rem 0",
+      width: "100%",
+      "&:focus": {
+        outline: "none",
+      },
+      "&::placeholder": {
+        opacity: 1.0,
+      },
+    },
+    "& #mc_embed_signup .button": {
+      background: "initial",
+      outline: "none",
+      backgroundImage: `url('${email.src}')`,
+      backgroundRepeat: "no-repeat",
+      backgroundSize: `${typography.pxToRem(48)} ${typography.pxToRem(48)}`,
+      border: "none",
+      height: typography.pxToRem(48),
+      padding: 0,
+      width: typography.pxToRem(48),
+      "&:hover , &:focus": {
+        background: "initial",
+        cursor: "pointer",
+        outline: "none",
+        backgroundImage: `url('${email.src}')`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: `${typography.pxToRem(48)} ${typography.pxToRem(48)}`,
+        border: "none",
+      },
     },
   },
 }));
@@ -24,25 +80,21 @@ function Newsletter({ description, placeholder, title, embedCode, ...props }) {
 
   return (
     <div className={classes.root}>
-      <Section classes={{ root: classes.section }}>
-        <Grid container justify="space-between" alignItems="stretch">
-          <Grid item xs={12} lg={4} container alignItems="center">
-            <div className={classes.textContainer}>
-              <Typography variant="h1" className={classes.title}>
-                {title}
-              </Typography>
-              {description && (
-                <Typography variant="body1" className={classes.description}>
-                  {description}
-                </Typography>
-              )}
-              <RichTypography classes={{ root: classes.form }}>
-                {embedCode}
-              </RichTypography>
-            </div>
-          </Grid>
-        </Grid>
-      </Section>
+      <Grid item xs={12} container alignItems="center">
+        <div className={classes.titleContainer}>
+          <Typography variant="h4" className={classes.title}>
+            {title}
+          </Typography>
+        </div>
+        {description && (
+          <Typography variant="body1" className={classes.description}>
+            {description}
+          </Typography>
+        )}
+        <RichTypography classes={{ root: classes.form }}>
+          {embedCode}
+        </RichTypography>
+      </Grid>
     </div>
   );
 }
