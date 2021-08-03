f28ae13cfb627e7d0dddcb0353615f30f30deda4
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-08-03 08:59:14 +0300

    add RT styling for visual card

--- src/components/DataVisualCard/Content.js
+++ src/components/DataVisualCard/Content.js
@@ -2,11 +2,20 @@ import { RichTypography } from "@commons-ui/core";
 import PropTypes from "prop-types";
 import React from "react";
 
-const Content = ({ description }) => (
-  <RichTypography variant="subtitle2" display="inline">
-    {description}
-  </RichTypography>
-);
+import useStyles from "./useStyles";
+
+const Content = ({ description, ...props }) => {
+  const classes = useStyles(props);
+  return (
+    <RichTypography
+      className={classes.content}
+      variant="subtitle2"
+      display="inline"
+    >
+      {description}
+    </RichTypography>
+  );
+};
 
 Content.propTypes = {
   description: PropTypes.string,
