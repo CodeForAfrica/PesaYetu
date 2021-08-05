730e363a4ad58c4fe78d1f32d9bb0c52d2c3f5d6
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-08-05 14:37:09 +0300

    re-add section + image proptype change

--- src/components/DataIndicators/index.js
+++ src/components/DataIndicators/index.js
@@ -5,6 +5,8 @@ import React, { useState } from "react";
 
 import useStyles from "./useStyles";
 
+import Section from "@/pesayetu/components/Section";
+
 const DataIndicators = ({ title, items, ...props }) => {
   const [checked, setChecked] = useState(false);
   const [currentTitle, setCurrentTitle] = useState("");
@@ -19,49 +21,53 @@ const DataIndicators = ({ title, items, ...props }) => {
 
   return (
     <div className={classes.root}>
-      <Grid container className={classes.container}>
-        <Grid container className={classes.indicatorsContainer}>
-          <Typography className={classes.sectionTitle}>{title}</Typography>
-          <div className={classes.iconContainer}>
-            {items?.map((item) => (
-              <Grid item className={classes.item}>
-                <div className={classes.imageContainer}>
-                  <Image
-                    className={classes.image}
-                    src={item.image}
-                    layout="fill"
-                    onClick={() => handleChange(item)}
-                  />
-                </div>
-                <Typography className={classes.text}>{item.title}</Typography>
-              </Grid>
-            ))}
-          </div>
+      <Section>
+        <Grid container className={classes.container}>
+          <Grid container className={classes.indicatorsContainer}>
+            <Typography className={classes.sectionTitle}>{title}</Typography>
+            <div className={classes.iconContainer}>
+              {items?.map((item) => (
+                <Grid key={item.title} item className={classes.item}>
+                  <div className={classes.imageContainer}>
+                    <Image
+                      className={classes.image}
+                      src={item.image}
+                      layout="fill"
+                      onClick={() => handleChange(item)}
+                    />
+                  </div>
+                  <Typography className={classes.text}>{item.title}</Typography>
+                </Grid>
+              ))}
+            </div>
+          </Grid>
+          <Grow
+            in={checked}
+            onClick={handleChange}
+            className={classes.transition}
+          >
+            <div className={classes.descriptionSection}>
+              <Typography className={classes.title}>{currentTitle}</Typography>
+              <Typography className={classes.description}>
+                {currentDescription}
+              </Typography>
+            </div>
+          </Grow>
         </Grid>
-        <Grow
-          in={checked}
-          onClick={handleChange}
-          className={classes.transition}
-        >
-          <div className={classes.descriptionSection}>
-            <Typography className={classes.title}>{currentTitle}</Typography>
-            <Typography className={classes.description}>
-              {currentDescription}
-            </Typography>
-          </div>
-        </Grow>
-      </Grid>
+      </Section>
     </div>
   );
 };
 
 DataIndicators.propTypes = {
   title: PropTypes.string,
-  items: PropTypes.arrayOf({
-    image: PropTypes.string,
-    title: PropTypes.string,
-    string: PropTypes.string,
-  }),
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      image: PropTypes.string,
+      title: PropTypes.string,
+      description: PropTypes.string,
+    })
+  ),
 };
 
 DataIndicators.defaultProps = {
