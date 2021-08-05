26e5ff3347f63ef40e08ecde1f3d7dab4719d78c
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-08-05 12:42:00 +0300

    update display info

--- src/components/DataIndicators/index.js
+++ src/components/DataIndicators/index.js
@@ -5,26 +5,15 @@ import React, { useState } from "react";
 
 import useStyles from "./useStyles";
 
-// import { dataIndicator } from "@/pesayetu/config";
-
 const DataIndicators = ({ title, items, ...props }) => {
   const [checked, setChecked] = useState(false);
+  const [currentTitle, setCurrentTitle] = useState("");
+  const [currentDescription, setCurrentDescription] = useState("");
   const classes = useStyles({ checked, ...props });
 
-  // const indicatorMap = {
-  //   Overview:
-  //     "This includes general county data. Topics include administrative and political units, population size and composition, land use, tourism and wildlife, industry and trade, finance, and education.",
-  //   Revenue:
-  //     "This looks at the review of the implementation of the previous County Integrated Development Plan for the period of 2014 to 2017. Datasets include county revenue streams and expenditure analysis.",
-  //   Development:
-  //     "This highlights the county development priorities and strategies. It looks at the programmes and associated budgets the county has proposed for the period 2018 to 2022.",
-  //   Implement:
-  //     "This showcases the implementation framework for the county as published in the County Integrated Development Plan. It covers the resource mobilisation framework on the proposed and predicted revenue and expenditure.",
-  //   Summary:
-  //     "The summary takes a look at the monitoring and evaluation framework as published in the County Integrated Development Plan. This includes the outcome indicators for each of the sector plans proposed.",
-  // };
-
-  const handleChange = () => {
+  const handleChange = ({ description, title: itemTitle }) => {
+    setCurrentTitle(itemTitle);
+    setCurrentDescription(description);
     setChecked((prev) => !prev);
   };
 
@@ -32,9 +21,7 @@ const DataIndicators = ({ title, items, ...props }) => {
     <div className={classes.root}>
       <Grid container className={classes.container}>
         <Grid container className={classes.indicatorsContainer}>
-          <Typography className={classes.sectionTitle}>
-            Data Indicators
-          </Typography>
+          <Typography className={classes.sectionTitle}>{title}</Typography>
           <div className={classes.iconContainer}>
             {items?.map((item) => (
               <Grid item className={classes.item}>
@@ -43,6 +30,7 @@ const DataIndicators = ({ title, items, ...props }) => {
                     className={classes.image}
                     src={item.image}
                     layout="fill"
+                    onClick={() => handleChange(item)}
                   />
                 </div>
                 <Typography className={classes.text}>{item.title}</Typography>
@@ -56,11 +44,9 @@ const DataIndicators = ({ title, items, ...props }) => {
           className={classes.transition}
         >
           <div className={classes.descriptionSection}>
-            <Typography className={classes.title}>Overview</Typography>
+            <Typography className={classes.title}>{currentTitle}</Typography>
             <Typography className={classes.description}>
-              This includes general county data. Topics include administrative
-              and political units, population size and composition, land use,
-              tourism and wildlife, industry and trade, finance, and education.
+              {currentDescription}
             </Typography>
           </div>
         </Grow>
