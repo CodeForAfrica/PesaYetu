3bb10a0d366935f2d864c835e987d728cd98d8b6
_ Kilemensi <clemence@codeforafrica.org>
2021-08-10 18:09:34 +0300

    Update stories

--- src/components/Tabs/index.stories.js
+++ src/components/Tabs/index.stories.js
@@ -2,20 +2,13 @@ import React from "react";
 
 import Tabs from ".";
 
-const tabItems = [
-  { label: "News", children: "This is news section" },
-  { label: "Insight", children: "This is the insight section" },
+const items = [
+  { label: "News", children: "This is news panel" },
+  { label: "Insights", children: "This is the insights panel" },
 ];
 
 export default {
   title: "Components/Tabs",
-  argTypes: {
-    tabItems: {
-      control: {
-        type: "array",
-      },
-    },
-  },
 };
 
 const Template = ({ ...args }) => <Tabs {...args} />;
@@ -23,5 +16,5 @@ const Template = ({ ...args }) => <Tabs {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  tabItems,
+  items,
 };
