aac9788a2137eb791096c27919605c9cff5e2552
Obed Amoasi <obedamoasi@gmail.com>
2021-08-10 09:29:46 +0000

    center text

--- src/components/AboutHero/useStyles.js
+++ src/components/AboutHero/useStyles.js
@@ -5,7 +5,7 @@ const useStyles = makeStyles(({ breakpoints, typography }) => ({
     height: typography.pxToRem(540),
     margin: 0,
     position: "relative",
-    [breakpoints.up("md")]: {
+    [breakpoints.up("lg")]: {
       height: typography.pxToRem(600),
     },
   },
@@ -14,7 +14,7 @@ const useStyles = makeStyles(({ breakpoints, typography }) => ({
     height: typography.pxToRem(540),
     width: "100%",
     zIndex: -1,
-    [breakpoints.up("md")]: {
+    [breakpoints.up("lg")]: {
       height: typography.pxToRem(600),
     },
   },
@@ -30,7 +30,7 @@ const useStyles = makeStyles(({ breakpoints, typography }) => ({
   textContainer: {
     height: "100%",
     paddingTop: typography.pxToRem(43),
-    [breakpoints.up("lg")]: {
+    [breakpoints.up("md")]: {
       paddingTop: typography.pxToRem(74),
     },
   },
@@ -57,13 +57,15 @@ const useStyles = makeStyles(({ breakpoints, typography }) => ({
     left: 0,
     top: typography.pxToRem(70),
     width: "100%",
+    height: typography.pxToRem(400),
     background: `linear-gradient(to right, #ffffffE6 0%, #ffffffE6 87.5%, transparent 87.5%, transparent 100%)`,
     [breakpoints.up("md")]: {
+      height: typography.pxToRem(352),
       background: `linear-gradient(to right, #ffffffE6 0%, #ffffffE6 62.5%, transparent 62.5%, transparent 100%)`,
     },
-    height: typography.pxToRem(400),
     [breakpoints.up("lg")]: {
       top: typography.pxToRem(100),
+      height: typography.pxToRem(400),
       background: `linear-gradient(to right, #ffffffE6 0%, #ffffffE6 56%, transparent 56%, transparent 100%)`,
     },
   },
