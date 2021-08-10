b87aea3a741e638e549f11eb19771a6dee2b9a95
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-08-10 09:14:16 +0300

    stories fix

--- src/components/AboutTeam/index.stories.js
+++ src/components/AboutTeam/index.stories.js
@@ -1,16 +1,15 @@
-/* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 
-import DataVisuals from ".";
+import AboutTeam from ".";
 
-import { dataVisuals } from "@/pesayetu/config";
+import { aboutTeam } from "@/pesayetu/config";
 
 export default {
   title: "Sections/About Team",
 };
 
-const Template = ({ ...args }) => <DataVisuals {...args} />;
+const Template = ({ ...args }) => <AboutTeam {...args} />;
 
 export const Default = Template.bind({});
 
-Default.args = dataVisuals;
+Default.args = aboutTeam;
