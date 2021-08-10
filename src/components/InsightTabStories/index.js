739ff9ecd99915e24686901cab5459e528d4eb92
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2021-08-10 15:34:10 +0300

    Minor changes

--- src/components/InsightTabStories/index.js
+++ src/components/InsightTabStories/index.js
@@ -2,11 +2,14 @@ import { Grid } from "@material-ui/core";
 import PropTypes from "prop-types";
 import React from "react";
 
+import useStyles from "./useStyles";
+
 import InsightsData from "@/pesayetu/components/InsightsData";
 
-function InsightTabStories({ insightProps }) {
+function InsightTabStories({ insightProps, ...props }) {
+  const classes = useStyles(props);
   return (
-    <Grid container>
+    <Grid container className={classes.root}>
       <Grid container item>
         <InsightsData {...insightProps} />
       </Grid>
---
+++ src/components/InsightTabStories/useStyles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    backgroundColor: "white",
+  },
+}));
+
+export default useStyles;
