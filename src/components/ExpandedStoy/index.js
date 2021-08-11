e85fd66240e67024ac01f22ea9bf5f02cc02dd32
khadija <khadijamahanga@y7mail.com>
2021-08-11 15:22:59 +0300

    styling & missing components

--- src/components/ExpandedStoy/index.js
+++ src/components/ExpandedStoy/index.js
@@ -9,83 +9,121 @@ import useStyles from "./useStyles";
 import Section from "@/pesayetu/components/Section";
 import ShareBar from "@/pesayetu/components/ShareBar";
 
+const dateOptions = {
+  year: "numeric",
+  month: "long",
+};
+
 function ExpandedStory({
   title,
+  author,
   date,
   content,
   children,
-  image,
+  featuredImage,
   socialLinks,
-  category,
+  categories,
   ...props
 }) {
   const classes = useStyles(props);
 
-  if (!content?.length && !image) {
-    return null;
+  let authorName = `${author?.node?.firstName ?? ""} ${
+    author?.node?.lastName ?? ""
+  }`;
+  if (authorName?.length < 2) {
+    authorName = author?.node?.nickname ?? author?.node?.slug;
   }
+  const image = featuredImage?.node?.sourceUrl;
+
+  const category = categories.edges[0]?.node?.name;
   return (
     <>
       <div className={classes.headerRoot}>
         <Section classes={{ root: classes.section }}>
-          {category && (
-            <Typography
-              color="primary"
-              variant="h1"
-              className={classes.overline}
-            >
-              {category}
-            </Typography>
-          )}
-          {title && (
-            <Typography color="primary" variant="h1" className={classes.title}>
-              {title}
-            </Typography>
-          )}
+          <Grid container className={classes.root}>
+            <Grid item lg={1} />
+            <Grid item xs={12} lg={10}>
+              {category && (
+                <Typography variant="overline">{category}</Typography>
+              )}
+              {title && (
+                <Typography variant="h1" className={classes.title}>
+                  {title}
+                </Typography>
+              )}
+              {date && (
+                <Typography className={classes.displayFlex} variant="overline">
+                  {new Date(date).toLocaleString("en-GB", dateOptions)}
+                </Typography>
+              )}
+              {authorName && (
+                <Typography className={classes.displayFlex} variant="overline">
+                  {authorName}
+                </Typography>
+              )}
+            </Grid>
+          </Grid>
         </Section>
       </div>
-      <div className={classes.sectionRoot} />
-      <Section>
-        <Grid container direction="column" className={classes.root}>
-          {date && (
-            <Typography className={classes.date} variant="h4">
-              {date}
-            </Typography>
-          )}
-          <Grid item md={8}>
-            {image && (
-              <div className={classes.image}>
-                <Image
-                  alt="article"
-                  src={image}
-                  layout="fill"
-                  objectFit="cover"
-                />
-              </div>
-            )}
-            <ShareBar title={title} socialLinks={socialLinks}>
-              Share
-            </ShareBar>
-            {content && (
-              <RichTypography className={classes.content} variant="body1">
-                {content}
-              </RichTypography>
-            )}
+      <div className={classes.contentRoot}>
+        <Section>
+          <Grid container>
+            <Grid item xs={false} lg={1} />
+            <Grid item xs={12} lg={7}>
+              {image && (
+                <div className={classes.image}>
+                  <Image
+                    alt="article"
+                    src={image}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+              )}
+              <ShareBar title={title} socialLinks={socialLinks}>
+                Share
+              </ShareBar>
+              {content && (
+                <RichTypography className={classes.content} variant="body1">
+                  {content}
+                </RichTypography>
+              )}
+            </Grid>
           </Grid>
-        </Grid>
-      </Section>
+        </Section>
+      </div>
     </>
   );
 }
 
 ExpandedStory.propTypes = {
   title: PropTypes.string,
-  socialLinks: PropTypes.arrayOf(PropTypes.string),
+  socialLinks: PropTypes.arrayOf(PropTypes.shape({})),
   date: PropTypes.string,
   content: PropTypes.string,
   children: PropTypes.string,
-  image: PropTypes.string,
-  category: PropTypes.string,
+  featuredImage: PropTypes.shape({
+    node: PropTypes.shape({
+      sourceUrl: PropTypes.string,
+    }),
+  }),
+  categories: PropTypes.shape({
+    edges: PropTypes.arrayOf(
+      PropTypes.shape({
+        node: PropTypes.shape({
+          name: PropTypes.string,
+        }),
+      })
+    ),
+  }),
+  author: PropTypes.shape({
+    node: PropTypes.shape({
+      firstName: PropTypes.string,
+      lastName: PropTypes.string,
+      nickname: PropTypes.string,
+      slug: PropTypes.string,
+    }),
+  }),
 };
 
 ExpandedStory.defaultProps = {
@@ -94,8 +132,9 @@ ExpandedStory.defaultProps = {
   content: undefined,
   children: undefined,
   socialLinks: undefined,
-  image: undefined,
-  category: undefined,
+  featuredImage: undefined,
+  categories: undefined,
+  author: undefined,
 };
 
 export default ExpandedStory;
--- src/components/ExpandedStoy/useStyles.js
+++ src/components/ExpandedStoy/useStyles.js
@@ -3,43 +3,38 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(({ typography, breakpoints, palette }) => ({
   headerRoot: {
     backgroundColor: palette.background.paper,
-    margin: `${typography.pxToRem(40)} 0`,
+    padding: `${typography.pxToRem(40)} 0`,
     [breakpoints.up("md")]: {
-      margin: `${typography.pxToRem(60)} 0`,
+      padding: `${typography.pxToRem(60)} 0`,
     },
-    [breakpoints.up("md")]: {
-      margin: `${typography.pxToRem(100)} 0 ${typography.pxToRem(100)}`,
+    [breakpoints.up("lg")]: {
+      padding: `${typography.pxToRem(100)} 0 ${typography.pxToRem(73)}`,
     },
   },
   contentRoot: {
     backgorundColor: palette.background.default,
-  },
-  overline: {
-    fontWeight: "bold",
-    fontSize: typography.pxToRem(14),
-    marginBottom: typography.pxToRem(13),
-    textTransform: "uppercase",
+    padding: `${typography.pxToRem(60)} 0 ${typography.pxToRem(80)}`,
+    [breakpoints.up("md")]: {
+      paddingTop: typography.pxToRem(40),
+    },
   },
   title: {
     margin: `${typography.pxToRem(20)} 0`,
   },
-  date: {
-    marginBottom: typography.pxToRem(40),
-    [breakpoints.up("md")]: {
-      marginBottom: typography.pxToRem(46),
-    },
+  displayFlex: {
+    display: "flex",
   },
   image: {
     position: "relative",
-    height: typography.pxToRem(222),
+    height: typography.pxToRem(217),
     width: "100%",
-    marginBottom: typography.pxToRem(20),
+    marginBottom: typography.pxToRem(40),
     [breakpoints.up("md")]: {
       height: typography.pxToRem(320),
     },
     [breakpoints.up("lg")]: {
-      width: typography.pxToRem(792),
-      height: typography.pxToRem(526),
+      width: typography.pxToRem(768),
+      height: typography.pxToRem(476),
     },
   },
   content: {
