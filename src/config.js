7a812609b2254da1a951d17a53717ba6331fb1f5
_ Kilemensi <clemence@codeforafrica.org>
2021-08-11 14:57:48 +0300

    Have more than 6 stories

--- src/config.js
+++ src/config.js
@@ -303,6 +303,31 @@ export const insightDataTabStories = {
       linkdescription: "Read More",
       href: "www.test.com",
     },
+    {
+      title: "Dolor sit amet ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit, magnis mus etiam nam lectus lobortis varius ultrices, donec dapibus dui felis est penatibus.",
+      image: unsplashOne,
+      linkdescription: "Read More",
+      href: "www.test.com",
+    },
+    {
+      title: "Dolor sit amet ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit, magnis mus etiam nam lectus lobortis varius ultrices, donec dapibus dui felis est penatibus.",
+      image: unsplashTwo,
+      linkdescription: "Read More",
+      href: "www.test.com",
+    },
+
+    {
+      title: "Dolor sit amet ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit, magnis mus etiam nam lectus lobortis varius ultrices, donec dapibus dui felis est penatibus.",
+      image: unsplashThree,
+      linkdescription: "Read More",
+      href: "www.test.com",
+    },
   ],
 };
 
