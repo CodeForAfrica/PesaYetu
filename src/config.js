5043d0542e0be21f2450c2ecfe0d36fa01737f9a
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2021-08-11 21:05:22 +0300

    Add config items

--- src/config.js
+++ src/config.js
@@ -113,6 +113,30 @@ export const insightDataStories = {
       linkdescription: "Read More",
       href: "www.test.com",
     },
+    {
+      title: "Dolor sit amet ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit, magnis mus etiam nam lectus lobortis varius ultrices, donec dapibus dui felis est penatibus.",
+      image: unsplashOne,
+      linkdescription: "Read More",
+      href: "www.test.com",
+    },
+    {
+      title: "Dolor sit amet ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit, magnis mus etiam nam lectus lobortis varius ultrices, donec dapibus dui felis est penatibus.",
+      image: unsplashOne,
+      linkdescription: "Read More",
+      href: "www.test.com",
+    },
+    {
+      title: "Dolor sit amet ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit, magnis mus etiam nam lectus lobortis varius ultrices, donec dapibus dui felis est penatibus.",
+      image: unsplashOne,
+      linkdescription: "Read More",
+      href: "www.test.com",
+    },
   ],
 };
 
