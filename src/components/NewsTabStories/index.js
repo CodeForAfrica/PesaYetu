888929b7aa94dae697bd6f9628d5e138b9b5649a
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2021-08-11 13:58:21 +0300

    Update carousel

--- src/components/NewsTabStories/index.js
+++ src/components/NewsTabStories/index.js
@@ -3,6 +3,7 @@ import { Grid, useMediaQuery } from "@material-ui/core";
 import { useTheme } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import React from "react";
+import Carousel from "react-multi-carousel";
 
 import useStyles from "./useStyles";
 
@@ -10,6 +11,24 @@ import FeaturedStoryCard from "@/pesayetu/components/FeaturedStoryCard";
 import InsightCard from "@/pesayetu/components/InsightCard";
 import Section from "@/pesayetu/components/Section";
 import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: {
+      max: 3000,
+      min: 1280,
+    },
+    items: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1279, min: 768 },
+    items: 1,
+  },
+  mobile: {
+    breakpoint: { max: 767, min: 0 },
+    items: 1,
+  },
+};
 
 function NewsTabStories({ featuredStoryProps, items, ...props }) {
   const classes = useStyles(props);
@@ -22,12 +41,21 @@ function NewsTabStories({ featuredStoryProps, items, ...props }) {
         <Grid item>
           <FeaturedStoryCard {...featuredStoryProps} />
         </Grid>
-        <Grid container item className={classes.stories}>
-          {showItemsData &&
-            showItemsData?.map((item) => (
-              <InsightCard key={item.title} {...item} />
-            ))}
-        </Grid>
+        <Carousel
+          swipeable
+          responsive={responsive}
+          arrows={false}
+          renderDotsOutside
+          showDots
+          dotListClass={classes.dots}
+        >
+          <Grid container item className={classes.stories}>
+            {showItemsData &&
+              showItemsData?.map((item) => (
+                <InsightCard key={item.title} {...item} />
+              ))}
+          </Grid>
+        </Carousel>
       </Section>
     </Grid>
   );
