d885622beea02b17d8bae78516c9da3b537c0ee0
Obed Amoasi <obedamoasi@gmail.com>
2021-08-12 01:05:18 +0000

    add Start Learning Block

--- src/pages/how-it-works.js
+++ src/pages/how-it-works.js
@@ -3,6 +3,7 @@ import React from "react";
 
 import Hero from "@/pesayetu/components/OtherHero";
 import Page from "@/pesayetu/components/Page";
+import StartLearning from "@/pesayetu/components/StartLearning";
 import formatBlocksForSections from "@/pesayetu/functions/formatBlocksForSections";
 import getPostTypeStaticProps from "@/pesayetu/functions/postTypes/getPostTypeStaticProps";
 
@@ -10,6 +11,7 @@ export default function Home({ blocks, ...props }) {
   return (
     <Page {...props}>
       <Hero {...blocks?.otherHero} />
+      <StartLearning {...blocks?.startLearning} />
     </Page>
   );
 }
@@ -17,6 +19,7 @@ export default function Home({ blocks, ...props }) {
 Home.propTypes = {
   blocks: PropTypes.shape({
     otherHero: PropTypes.shape({}),
+    startLearning: PropTypes.shape({}),
   }),
 };
 
