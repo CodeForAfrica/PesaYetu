f3c60fe3340c597fc6f4d52ca1df49fb6f401d68
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2021-08-23 14:39:52 +0300

    Initial layout

--- src/components/Documents/index.js
+++ src/components/Documents/index.js
@@ -1,7 +1,27 @@
+import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 
-function Documents() {
-  return <div>this is an example document</div>;
+import useStyles from "./useStyles";
+
+function Documents({ ...props }) {
+  const classes = useStyles(props);
+  return (
+    <Grid
+      container
+      direction="row"
+      alignItems="center"
+      justifyContent="space-between"
+      className={classes.root}
+    >
+      <Grid container direction="row" justifyContent="flex-start" item xs={8}>
+        <Typography variant="body1">this is item one</Typography>
+        <Typography variant="body1">this is item two</Typography>
+      </Grid>
+      <Grid item xs={4}>
+        <Typography variant="body1">Read more</Typography>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Documents;
---
+++ src/components/Documents/useStyles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "100%",
+    height: "120px",
+    background: "#FFFFFF",
+    border: "1px solid #DFDFDF",
+    opacity: 1,
+  },
+}));
+
+export default useStyles;
