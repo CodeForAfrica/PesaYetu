94a13cc5cc89e258d4b675ba3bec7b383991716e
Obed Amoasi <obedamoasi@gmail.com>
2021-08-24 12:26:55 +0000

    setup page

---
+++ src/pages/data.js
@@ -0,0 +1,52 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import DataSource from "@/pesayetu/components/DataSource";
+import Hero from "@/pesayetu/components/OtherHero";
+import Page from "@/pesayetu/components/Page";
+import formatBlocksForSections from "@/pesayetu/functions/formatBlocksForSections";
+import getPostTypeStaticProps from "@/pesayetu/functions/postTypes/getPostTypeStaticProps";
+
+export default function Data({ blocks, ...props }) {
+  return (
+    <Page {...props}>
+      <Hero {...blocks?.otherHero} />
+      <DataSource />
+    </Page>
+  );
+}
+
+Data.propTypes = {
+  blocks: PropTypes.shape({
+    otherHero: PropTypes.shape({}),
+  }),
+};
+
+Data.defaultProps = {
+  blocks: undefined,
+};
+
+export async function getStaticProps({ preview, previewData }) {
+  const postType = "page";
+  const { props, revalidate, notFound } = await getPostTypeStaticProps(
+    { slug: "/data" },
+    postType,
+    preview,
+    previewData
+  );
+
+  if (notFound) {
+    return {
+      notFound,
+    };
+  }
+
+  const blocks = formatBlocksForSections(props?.post?.blocks);
+  return {
+    props: {
+      ...props,
+      blocks,
+    },
+    revalidate,
+  };
+}
