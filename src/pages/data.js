6a0d51ad308a6e9a7cb0ac66cc0b94e91fdf9642
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2021-09-01 13:01:18 +0300

    Add to pages

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,17 +1,25 @@
 import PropTypes from "prop-types";
 import React from "react";
 
+import DatasetsAndDocuments from "@/pesayetu/components/DatasetsAndDocuments";
 import DataSources from "@/pesayetu/components/DataSources";
 import Hero from "@/pesayetu/components/OtherHero";
 import Page from "@/pesayetu/components/Page";
+import { datasetTypeArgs, documentsArg } from "@/pesayetu/config";
 import formatBlocksForSections from "@/pesayetu/functions/formatBlocksForSections";
 import getPostTypeStaticProps from "@/pesayetu/functions/postTypes/getPostTypeStaticProps";
+
+const items = [
+  { label: "DOCUMENTS & SPEECHES", children: documentsArg },
+  { label: "DATASET", children: datasetTypeArgs },
+];
 
 export default function Data({ blocks, ...props }) {
   return (
     <Page {...props}>
       <Hero {...blocks?.otherHero} />
       <DataSources {...blocks?.dataSource} />
+      <DatasetsAndDocuments items={items} />
     </Page>
   );
 }
