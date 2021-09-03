d61dfdf332c1661feb396629f2780dbf8179eb9c
_ Kilemensi <clemence@codeforafrica.org>
2021-09-03 15:01:23 +0300

    Switch to using custom Carousel component

--- src/components/ExploreOtherTools/index.js
+++ src/components/ExploreOtherTools/index.js
@@ -1,31 +1,12 @@
 import { Typography } from "@material-ui/core";
 import PropTypes from "prop-types";
 import React from "react";
-import Carousel from "react-multi-carousel";
 
 import useStyles from "./useStyles";
 
 import Card from "@/pesayetu/components/Card";
+import Carousel from "@/pesayetu/components/Carousel";
 import Section from "@/pesayetu/components/Section";
-import "react-multi-carousel/lib/styles.css";
-
-const responsive = {
-  desktop: {
-    breakpoint: {
-      max: 3000,
-      min: 1280,
-    },
-    items: 3,
-  },
-  tablet: {
-    breakpoint: { max: 1279, min: 768 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 767, min: 0 },
-    items: 1,
-  },
-};
 
 const ExploreOtherTools = ({ title, items, ...props }) => {
   const classes = useStyles(props);
@@ -40,14 +21,7 @@ const ExploreOtherTools = ({ title, items, ...props }) => {
           {title}
         </Typography>
 
-        <Carousel
-          swipeable
-          responsive={responsive}
-          arrows={false}
-          renderDotsOutside
-          showDots
-          dotListClass={classes.dots}
-        >
+        <Carousel>
           {items.map((item) => (
             <Card
               key={item.title}
