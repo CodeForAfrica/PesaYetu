fc66b43b0985e0304790ba43aa36633c9f03c30b
_ Kilemensi <clemence@codeforafrica.org>
2021-09-03 15:01:47 +0300

    Switch to using custom Carousel component

--- src/components/OurCourses/index.js
+++ src/components/OurCourses/index.js
@@ -2,37 +2,21 @@ import { Typography, useMediaQuery } from "@material-ui/core";
 import { useTheme } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import React from "react";
-import Carousel from "react-multi-carousel";
 
 import useStyles from "./useStyles";
 
 import Card from "@/pesayetu/components/Card";
+import Carousel from "@/pesayetu/components/Carousel";
 import Section from "@/pesayetu/components/Section";
-import "react-multi-carousel/lib/styles.css";
-
-const responsive = {
-  desktop: {
-    breakpoint: {
-      max: 3000,
-      min: 1280,
-    },
-    items: 3,
-  },
-  tablet: {
-    breakpoint: { max: 1279, min: 768 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 767, min: 0 },
-    items: 1,
-  },
-};
 
 const OurCourses = ({ title, items, ...props }) => {
   const classes = useStyles(props);
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("lg"));
 
+  if (!items?.length) {
+    return null;
+  }
   return (
     <div className={classes.root}>
       <Section classes={{ root: classes.section }}>
@@ -41,15 +25,8 @@ const OurCourses = ({ title, items, ...props }) => {
             {title}
           </Typography>
         )}
-        <Carousel
-          swipeable
-          responsive={responsive}
-          arrows={false}
-          renderDotsOutside
-          showDots={!isDesktop}
-          dotListClass={classes.dots}
-        >
-          {items?.map((item) => (
+        <Carousel showDots={!isDesktop} classes={{ dotList: classes.dotList }}>
+          {items.map((item) => (
             <Card key={item.title} {...item} />
           ))}
         </Carousel>
--- src/components/OurCourses/useStyles.js
+++ src/components/OurCourses/useStyles.js
@@ -5,9 +5,7 @@ const useStyles = makeStyles(({ typography, breakpoints, palette }) => ({
     backgroundColor: palette.grey.main,
   },
   section: {
-    padding: `${typography.pxToRem(60)} ${typography.pxToRem(
-      20
-    )} 0 ${typography.pxToRem(20)}`,
+    padding: `${typography.pxToRem(60)} ${typography.pxToRem(20)}`,
     [breakpoints.up("md")]: {
       padding: `${typography.pxToRem(80)} 0`,
     },
@@ -16,24 +14,13 @@ const useStyles = makeStyles(({ typography, breakpoints, palette }) => ({
     marginBottom: typography.pxToRem(40),
     textAlign: "center",
   },
-  dots: {
-    margin: `0 ${typography.pxToRem(30)}`,
-    paddingTop: typography.pxToRem(40),
-    paddingBottom: typography.pxToRem(50),
-    position: "unset",
+  dotList: {
     "& button": {
       borderColor: "#000",
-      height: typography.pxToRem(16),
-      marginRight: typography.pxToRem(12),
-      width: typography.pxToRem(16),
       background: palette.grey.main,
     },
     "& .react-multi-carousel-dot--active button": {
       borderColor: "#000",
-      background: "#000",
-    },
-    [breakpoints.up("md")]: {
-      paddingBottom: typography.pxToRem(0),
     },
   },
 }));
