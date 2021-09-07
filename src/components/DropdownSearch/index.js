6e259a055ced65d19b8185436aa20a2623cf2651
khadija <khadijamahanga@y7mail.com>
2021-09-07 11:44:41 +0300

    remove console

--- src/components/DropdownSearch/index.js
+++ src/components/DropdownSearch/index.js
@@ -119,8 +119,6 @@ function DropdownSearch({
     </IconButton>
   );
 
-  console.log(variant);
-
   return (
     <div className={classes.root}>
       <Typography variant="body1" className={classes.label}>
