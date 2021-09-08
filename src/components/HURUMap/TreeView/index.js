b8bd85dd64bb8c470d23abbf15d9408b3252adb4
Obed Amoasi <obedamoasi@gmail.com>
2021-09-08 11:20:41 +0000

    fix import error

--- src/components/HURUMap/TreeView/index.js
+++ src/components/HURUMap/TreeView/index.js
@@ -1,5 +1,6 @@
 import { Typography } from "@material-ui/core";
-import { TreeItem, Treeviw as MuiTreeView } from "@material-ui/lab";
+import TreeItem from "@material-ui/lab/TreeItem";
+import MuiTreeView from "@material-ui/lab/TreeView";
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import React from "react";
@@ -18,42 +19,40 @@ const TreeView = ({ items, ...props }) => {
   return (
     <div className={classes.root}>
       {items.map(({ children, label, path }) => (
-        <>
-          <MuiTreeView>
-            <TreeItem
-              endIcon={1}
-              label={
-                <Link underline="none" href={path}>
-                  <Typography className={classes.label} variant="caption">
-                    {label} <CheckIcon className={classes.icon} />
-                  </Typography>
-                </Link>
-              }
-              classes={{
-                root: classes.tree,
-                expanded: classes.expanded,
-              }}
-              nodeId={path}
-            >
-              {children.map((child) => (
-                <TreeItem
-                  key={child.path}
-                  nodeId={child.path}
-                  label={
-                    <Link underline="none" href={child.path}>
-                      <Typography
-                        className={clsx(classes.label, classes.childLabel)}
-                        variant="caption"
-                      >
-                        {child.label}
-                      </Typography>
-                    </Link>
-                  }
-                />
-              ))}
-            </TreeItem>
-          </MuiTreeView>
-        </>
+        <MuiTreeView>
+          <TreeItem
+            endIcon={1}
+            label={
+              <Link underline="none" href={path}>
+                <Typography className={classes.label} variant="caption">
+                  {label} <CheckIcon className={classes.icon} />
+                </Typography>
+              </Link>
+            }
+            classes={{
+              root: classes.tree,
+              expanded: classes.expanded,
+            }}
+            nodeId={path}
+          >
+            {children.map((child) => (
+              <TreeItem
+                key={child.path}
+                nodeId={child.path}
+                label={
+                  <Link underline="none" href={child.path}>
+                    <Typography
+                      className={clsx(classes.label, classes.childLabel)}
+                      variant="caption"
+                    >
+                      {child.label}
+                    </Typography>
+                  </Link>
+                }
+              />
+            ))}
+          </TreeItem>
+        </MuiTreeView>
       ))}
     </div>
   );
