4507f3bf077b9cbd06071ce755e25d98b9b5644e
Khadija Mahanga <khadijamahanga@y7mail.com>
2021-09-10 16:31:44 +0300

    Delete exit-preview.js

--- src/pages/api/exit-preview.js
+++
@@ -1,8 +0,0 @@
-/**
- * Exit preview mode and redirect to homepage.
- */
-export default async function exit(req, res) {
-  res.clearPreviewData();
-  res.writeHead(307, { Location: "/" });
-  res.end();
-}
