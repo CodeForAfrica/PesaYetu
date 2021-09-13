4470cf4c3d872cd0fcd7e38dd387b20acc28d1b8
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-09-13 17:12:25 +0200

    add icon

--- src/components/HURUmap/CategoryHeader/index.js
+++ src/components/HURUmap/CategoryHeader/index.js
@@ -1,13 +1,23 @@
-import { Typography } from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
+import Image from "next/image";
 import React from "react";
 
 import useStyles from "./useStyles";
+
+import Group4658 from "@/pesayetu/assets/icons/Group 4658-white.svg";
 
 const CategoryHeader = () => {
   const classes = useStyles();
   return (
     <div>
-      <Typography variant="h3">Overview</Typography>
+      <Grid container>
+        <div className={classes.image}>
+          <Image src={Group4658} layout="fill" />
+        </div>
+        <Typography variant="h3" className={classes.heading}>
+          Overview
+        </Typography>
+      </Grid>
       <Typography variant="body2" className={classes.description}>
         Population, Political, Land Use Type, Agriculture, Industries & Trade,
         Health Access, Education And Literacy
--- src/components/HURUmap/CategoryHeader/useStyles.js
+++ src/components/HURUmap/CategoryHeader/useStyles.js
@@ -2,8 +2,14 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(({ typography }) => ({
   root: {},
+  image: {
+    position: "relative",
+    height: typography.pxToRem(44.4),
+    width: typography.pxToRem(44.4),
+  },
   description: {
     padding: `${typography.pxToRem(20)} 0`,
+    fontWeight: 400,
   },
 }));
 
