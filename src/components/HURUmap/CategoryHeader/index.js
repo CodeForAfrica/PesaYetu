1f43e7f6974f7e79ab20776bd134fc7eb680be95
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-09-13 16:05:01 +0200

    add content

--- src/components/HURUmap/CategoryHeader/index.js
+++ src/components/HURUmap/CategoryHeader/index.js
@@ -1,5 +1,19 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 
-const CategoryHeader = () => <div>Category header</div>;
+import useStyles from "./useStyles";
+
+const CategoryHeader = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Typography variant="h3">Overview</Typography>
+      <Typography variant="body2" className={classes.description}>
+        Population, Political, Land Use Type, Agriculture, Industries & Trade,
+        Health Access, Education And Literacy
+      </Typography>
+    </div>
+  );
+};
 
 export default CategoryHeader;
--- src/components/HURUmap/CategoryHeader/useStyles.js
+++ src/components/HURUmap/CategoryHeader/useStyles.js
@@ -1,7 +1,10 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(({ typography }) => ({
   root: {},
+  description: {
+    padding: `${typography.pxToRem(20)} 0`,
+  },
 }));
 
 export default useStyles;
