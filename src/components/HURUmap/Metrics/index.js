ac0096248c1e119c492192cade7402473efbfd18
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-09-13 10:39:37 +0200

    add base files

---
+++ src/components/HURUmap/Metrics/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Metric = () => <div>Indicator Metrics</div>;
+
+export default Metric;
---
+++ src/components/HURUmap/Metrics/index.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Metrics from "@/pesayetu/components/HURUmap/Metrics";
+
+export default {
+  title: "Components/HURUmap/Metrics",
+  argTypes: {},
+};
+
+const Template = () => <Metrics />;
+
+export const Default = Template.bind({});
+
+Default.args = {};
