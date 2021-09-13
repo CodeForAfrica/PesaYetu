0a8b7758ade9579109e005f0c756586fd07f7b1a
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-09-13 15:43:50 +0200

    add base files

---
+++ src/components/HURUmap/CategoryHeader/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const CategoryHeader = () => <div>Category header</div>;
+
+export default CategoryHeader;
---
+++ src/components/HURUmap/CategoryHeader/index.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CategoryHeader from "@/pesayetu/components/HURUmap/CategoryHeader";
+
+export default {
+  title: "Components/HURUmap/CategoryHeader",
+  argTypes: {},
+};
+
+const Template = () => <CategoryHeader />;
+
+export const Default = Template.bind({});
+
+Default.args = {};
