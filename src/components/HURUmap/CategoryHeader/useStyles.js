6c5e1140a52fbd6c7c162323085121ecd023bdcc
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-09-14 16:55:37 +0200

    font changes

--- src/components/HURUmap/CategoryHeader/useStyles.js
+++ src/components/HURUmap/CategoryHeader/useStyles.js
@@ -3,14 +3,16 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(({ typography }) => ({
   root: {},
   heading: {
-    fontSize: typography.pxToRem(28), // no available header font of this size
+    fontSize: typography.pxToRem(28),
   },
   image: {
     position: "relative",
     height: typography.pxToRem(55.5),
     width: typography.pxToRem(55.5),
   },
-  description: {},
+  description: {
+    fontWeight: "400",
+  },
 }));
 
 export default useStyles;
