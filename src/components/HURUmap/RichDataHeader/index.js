480662204a085fd921067ecffb49e40c91af68ae
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-09-15 12:13:47 +0200

    base content

--- src/components/HURUmap/RichDataHeader/index.js
+++ src/components/HURUmap/RichDataHeader/index.js
@@ -1,5 +1,40 @@
+import {
+  Typography,
+  Grid,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
 import React from "react";
 
-const RichDataHeader = () => <div>Rich Data Header</div>;
+import useStyles from "./useStyles";
+
+const RichDataHeader = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container justifyContent="space-between">
+        <Typography>Isiolo</Typography>
+        <Typography>Print Icon</Typography>
+      </Grid>
+      <Typography>COUNTY IN KENYA</Typography>
+      <hr />
+      <Grid container>
+        <Typography>pin icon</Typography>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="temp-id">Age</InputLabel>
+          <Select labelId="temp-id" id="simple-select">
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </Grid>
+      <hr />
+    </div>
+  );
+};
 
 export default RichDataHeader;
