531a360706d6f85c4cd48a32bb16b7eea2bcad1e
_ Kilemensi <clemence@codeforafrica.org>
2021-09-20 15:09:19 +0300

    Include Location + update styles

--- src/components/ExplorePage/index.js
+++ src/components/ExplorePage/index.js
@@ -1,55 +1,76 @@
-import { Hidden } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import dynamic from "next/dynamic";
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
 
-import MapLocationTags from "@/pesayetu/components/HURUmap/MapLocationTags";
-import { items, tags } from "@/pesayetu/config";
+import Location from "@/pesayetu/components/HURUmap/Location";
+import { hurumapArgs } from "@/pesayetu/config";
 import fetchJson from "@/pesayetu/utils/fetchJson";
 
 const Map = dynamic(() => import("@/pesayetu/components/HURUmap/Map"), {
   ssr: false,
 });
 
-const useStyles = makeStyles(({ breakpoints, palette, typography }) => ({
-  root: {
-    position: "relative",
-    height: "calc(100vh - 88px)",
-    [breakpoints.up("lg")]: {
-      height: "calc(100vh - 110px)",
+const useStyles = makeStyles(
+  ({ breakpoints, palette, typography, zIndex }) => ({
+    root: {
+      position: "relative",
+      height: "calc(100vh - 88px)",
+      [breakpoints.up("lg")]: {
+        height: "calc(100vh - 110px)",
+      },
+      "& .tooltipPop": {
+        background: palette.background.default,
+        boxShadow: "0px 3px 6px #00000029",
+        height: typography.pxToRem(36),
+        width: typography.pxToRem(88),
+        "& .level": {
+          background: palette.primary.main,
+          borderRadius: typography.pxToRem(4),
+          color: palette.text.secondary,
+          display: "flex",
+          fontSize: typography.pxToRem(7),
+          fontWeight: "bold",
+          height: typography.pxToRem(17),
+          justifyContent: "center",
+          lineHeight: 10 / 7,
+          margin: "0 auto",
+          marginTop: typography.pxToRem(-15),
+          paddingTop: typography.pxToRem(2),
+          textTransform: "uppercase",
+          width: typography.pxToRem(62),
+        },
+        "& .name": {
+          textAlign: "center",
+          fontSize: typography.pxToRem(9),
+          fontWeight: "bold",
+          lineHeight: 13 / 9,
+          marginTop: typography.pxToRem(5),
+          textTransform: "capitalize",
+        },
+      },
     },
-    "& .tooltipPop": {
-      background: palette.background.default,
-      boxShadow: "0px 3px 6px #00000029",
-      height: typography.pxToRem(36),
-      width: typography.pxToRem(88),
-      "& .level": {
-        fontWeight: "bold",
-        color: palette.text.secondary,
-        background: palette.primary.main,
-        textTransform: "upperCase",
-        fontSize: typography.pxToRem(7),
-        marginTop: typography.pxToRem(-15),
-        borderRadius: typography.pxToRem(4),
-        paddingTop: typography.pxToRem(2),
-        width: typography.pxToRem(62),
-        height: typography.pxToRem(17),
-        margin: "0 auto",
+    map: {
+      display: "none",
+      [breakpoints.up("md")]: {
+        display: "block",
+      },
+    },
+    location: {
+      display: "none",
+      [breakpoints.up("md")]: {
         display: "flex",
-        justifyContent: "center",
-      },
-      "& .name": {
-        textAlign: "center",
-        fontSize: typography.pxToRem(9),
-        fontWeight: "bold",
-        textTransform: "capitalize",
-        marginTop: typography.pxToRem(5),
+        left: 0,
+        margin: "0 auto",
+        position: "absolute",
+        right: 0,
+        top: typography.pxToRem(52),
+        zIndex: zIndex.appBar,
       },
     },
-  },
-}));
+  })
+);
 
 function ExplorePage({
   geometries: geometriesProp,
@@ -62,8 +83,9 @@ function ExplorePage({
   const [geometries, setGeometries] = useState(geometriesProp);
   const [geography, setGeography] = useState(geographyProp);
   const [shouldFetch, setShouldFetch] = useState(false);
+  const { location: locationArgs } = hurumapArgs;
 
-  const { data } = useSWR(
+  const { data, error } = useSWR(
     shouldFetch
       ? `${apiUri}all_details/profile/1/geography/${geoCode}/?format=json`
       : null,
@@ -83,20 +105,24 @@ function ExplorePage({
       setGeography(g);
     }
   }, [data]);
+  const isLoading = !data && !error;
   return (
     <div className={classes.root}>
-      <Hidden smDown>
-        <MapLocationTags tags={tags} items={items} />
-        <Map
-          center={[0.3051933453207569, 37.908818734483155]}
-          zoom={6.25}
-          geometries={geometries}
-          geography={geography}
-          setShouldFetch={setShouldFetch}
-          setGeoCode={setGeoCode}
-          {...props}
-        />
-      </Hidden>
+      <Map
+        center={[0.3051933453207569, 37.908818734483155]}
+        zoom={6.25}
+        geometries={geometries}
+        geography={geography}
+        setShouldFetch={setShouldFetch}
+        setGeoCode={setGeoCode}
+        {...props}
+        className={classes.map}
+      />
+      <Location
+        {...locationArgs}
+        isLoading={isLoading}
+        className={classes.location}
+      />
     </div>
   );
 }
