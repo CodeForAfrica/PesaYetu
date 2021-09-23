4339c2745ad3e2742d47c4e810ca691066a5a1a8
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-09-23 12:09:36 +0200

    underline fix

--- src/components/HURUmap/SubcategoryHeader/index.js
+++ src/components/HURUmap/SubcategoryHeader/index.js
@@ -6,7 +6,6 @@ import React from "react";
 
 const useStyles = makeStyles(({ palette, typography }) => ({
   root: {
-    borderBottom: `solid 1px ${palette.divider}`,
     paddingBottom: typography.pxToRem(20),
   },
   title: {
@@ -19,6 +18,8 @@ const useStyles = makeStyles(({ palette, typography }) => ({
   },
   description: {
     marginTop: typography.pxToRem(20),
+    paddingBottom: typography.pxToRem(20),
+    borderBottom: `solid 1px ${palette.divider}`,
   },
 }));
 
