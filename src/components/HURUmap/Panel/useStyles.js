88acde6a46939da1b520bc9b809b287deee04d4d
Obed Amoasi <obedamoasi@gmail.com>
2021-09-24 13:27:22 +0000

    fix position of paper box

--- src/components/HURUmap/Panel/useStyles.js
+++ src/components/HURUmap/Panel/useStyles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(({ typography }) => ({
   root: {
     display: "flex",
-    position: "relative",
+    position: "fixed",
     width: "max-content",
   },
   paper: {
