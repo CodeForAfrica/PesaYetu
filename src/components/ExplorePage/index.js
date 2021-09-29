93248e8ba8a8d5fc03f7ddc7ee7270033805f1af
Obed Amoasi <obedamoasi@gmail.com>
2021-09-29 05:35:53 +0000

    pass right profile to panel

--- src/components/ExplorePage/index.js
+++ src/components/ExplorePage/index.js
@@ -109,7 +109,7 @@ function ExplorePage({ profile: profileProp, apiUri, ...props }) {
   return (
     <div className={classes.root}>
       <Hidden mdDown implementation="css">
-        <Panel {...panelArgs} {...profileProp} />
+        <Panel {...panelArgs} {...profile} />
       </Hidden>
       <Map
         center={[0.3051933453207569, 37.908818734483155]}
