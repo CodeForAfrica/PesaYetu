365ef4261e4dce98af60f1e4da6716be34dddedd
Obed Amoasi <obedamoasi@gmail.com>
2021-09-29 19:26:06 +0000

    add scroll behaviour on desktop on html

--- src/components/HURUmap/Panel/useStyles.js
+++ src/components/HURUmap/Panel/useStyles.js
@@ -1,11 +1,18 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(
-  ({ typography, palette, transitions, widths, zIndex }) => ({
+  ({ typography, palette, breakpoints, transitions, widths, zIndex }) => ({
     root: {
       display: "flex",
       height: "100%",
       scrollBehavior: "smooth",
+    },
+    [breakpoints.up("lg")]: {
+      "@global": {
+        html: {
+          scrollBehavior: "smooth",
+        },
+      },
     },
     drawerOpen: {
       minWidth: typography.pxToRem(1049),
