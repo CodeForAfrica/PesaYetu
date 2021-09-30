d94187719c99f2a80bb64d280999f9456d127a8a
khadijamahanga <khadijamahanga@y7mail.com>
2021-09-30 15:41:53 +0300

    add embed page

---
+++ src/pages/embed/[geoCode]/[chartId].js
@@ -0,0 +1,96 @@
+import { makeStyles } from "@material-ui/core/styles";
+import dynamic from "next/dynamic";
+import React from "react";
+
+import fetchJson from "@/pesayetu/utils/fetchJson";
+import fetchProfile from "@/pesayetu/utils/fetchProfile";
+import fetchProfileConfigurations from "@/pesayetu/utils/fetchProfileConfigurations";
+
+const Chart = dynamic(() => import("@/pesayetu/components/HURUmap/Chart"), {
+  ssr: false,
+});
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "100% !important",
+  },
+}));
+
+export default function Embed(props) {
+  const classes = useStyles();
+  return (
+    <div>
+      <Chart {...props} classes={{ root: classes.root }} />
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const apiUri = process.env.HURUMAP_API_URL;
+  const { locationCodes } = await fetchProfileConfigurations();
+  const { data } = await fetchProfile(apiUri, "KE");
+
+  const subcategories = Object.values(data)?.map((l) => l.subcategories);
+  const profileIndicators = subcategories.reduce((acc, subcat) => {
+    const indicatorIds = Object.values(subcat).reduce((b, m) => {
+      let mIndicatorIds = [];
+      if (m?.indicators) {
+        mIndicatorIds = Object.values(m.indicators).map(({ id }) => id);
+      }
+      return b.concat(mIndicatorIds);
+    }, []);
+
+    return acc.concat(indicatorIds);
+  }, []);
+
+  const paths = locationCodes.reduce((acc, locationCode) => {
+    return acc.concat(
+      profileIndicators.map((pi) => {
+        return {
+          params: { geoCode: locationCode.toString(), chartId: pi.toString() },
+        };
+      })
+    );
+  }, []);
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps({ params: { geoCode, chartId } }) {
+  const apiUri = process.env.HURUMAP_API_URL;
+  const { indicator } = await fetchJson(
+    `${apiUri}profile/1/geography/${geoCode.toUpperCase()}/indicator/${chartId}/?format=json`
+  );
+
+  if (!indicator && JSON.stringify(indicator) === "{}") {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      title: indicator?.profile_indicator_label,
+      indicator: {
+        ...indicator,
+        chart_configuration: indicator?.indicator_chart_configuration,
+        id: indicator?.profile_indicator_id,
+        metadata: {
+          source: indicator?.metadata_source,
+          url: indicator?.metadata_url,
+          primary_group: indicator?.primary_group[0],
+          groups: Object.keys(indicator?.data[0])
+            .filter((g) => g !== "count")
+            ?.map((g) => {
+              return { name: g };
+            }),
+        },
+      },
+      geoCode: indicator?.geography_code,
+    },
+    revalidate: 60 * 5,
+  };
+}
