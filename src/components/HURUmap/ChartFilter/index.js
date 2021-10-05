083aa4257a1aebfa7d3947e2cbebe592655b55c3
khadija <khadijamahanga@y7mail.com>
2021-10-05 07:26:13 +0300

    update chart filter component

--- src/components/HURUmap/ChartFilter/index.js
+++ src/components/HURUmap/ChartFilter/index.js
@@ -1,37 +1,114 @@
+import { Grid } from "@material-ui/core";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import useStyles from "./useStyles";
 
 import Select from "@/pesayetu/components/Select";
 
-const ChartFilter = ({ label, options, selected, helperText, ...props }) => {
+function ChartFilter({
+  groups,
+  defaultFilter,
+  attributeText,
+  valueText,
+  ...props
+}) {
   const classes = useStyles(props);
+
+  const [filters, setFilters] = useState([]);
+  const [selectedAttribute, setSelectedAttribute] = useState("All values");
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const [attributeOptions, setAttributeOptions] = useState([]);
+  const [valueOptions, setValueOptions] = useState([]);
+
+  useEffect(() => {
+    if (defaultFilter?.name && defaultFilter?.value) {
+      setSelectedAttribute(defaultFilter?.name);
+      setSelectedValue(defaultFilter?.value);
+      setAttributeOptions([defaultFilter?.name]);
+      setValueOptions(
+        groups?.find(({ name }) => name === defaultFilter?.name)
+          ?.subindicators ?? [defaultFilter.value]
+      );
+    } else {
+      setFilters(groups);
+      setAttributeOptions(["All values", ...groups?.map((g) => g.name)]);
+    }
+  }, [groups, defaultFilter]);
+
+  const onAtrributeChange = (e) => {
+    if (e?.target?.value) {
+      setSelectedAttribute(e.target.value);
+      setValueOptions(
+        filters.find(({ name }) => name === e.target.value)?.subindicators
+      );
+    }
+  };
+
+  const onValueChange = (e) => {
+    if (e?.target?.value) {
+      setSelectedValue(e.target.value);
+    }
+  };
+
+  if (!groups?.length) {
+    return null;
+  }
+
   return (
     <div className={classes.root}>
-      <Select
-        helperText={helperText}
-        label={label}
-        classes={{ select: classes.select }}
-        options={options}
-        selected={selected}
-      />
+      <Grid container>
+        {attributeOptions?.length > 0 && (
+          <Grid item xs={12} md={6}>
+            <Select
+              helperText={attributeText}
+              options={attributeOptions}
+              selected={selectedAttribute}
+              onChange={onAtrributeChange}
+              disabled={!!defaultFilter}
+              classes={{ select: classes.select }}
+            />
+          </Grid>
+        )}
+        {valueOptions?.length > 0 && (
+          <Grid item xs={12} md={6}>
+            <Select
+              helperText={valueText}
+              options={valueOptions}
+              selected={selectedValue}
+              onChange={onValueChange}
+              classes={{ select: classes.select }}
+            />
+          </Grid>
+        )}
+      </Grid>
     </div>
   );
-};
+}
 
 ChartFilter.propTypes = {
-  helperText: PropTypes.string,
-  label: PropTypes.string,
-  selected: PropTypes.string,
-  options: PropTypes.arrayOf(PropTypes.shape({})),
+  groups: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      subindicators: PropTypes.arrayOf(PropTypes.string),
+    })
+  ),
+  defaultFilter: PropTypes.shape({
+    name: PropTypes.string,
+    value: PropTypes.string,
+  }),
+  view: PropTypes.shape({}),
+  attributeText: PropTypes.string,
+  valueText: PropTypes.string,
 };
 
 ChartFilter.defaultProps = {
-  helperText: undefined,
-  label: undefined,
-  selected: undefined,
-  options: undefined,
+  defaultFilter: undefined,
+  groups: undefined,
+  view: undefined,
+  attributeText: "Filter by attribute:",
+  valueText: "Select a value:",
 };
 
 export default ChartFilter;
--- src/components/HURUmap/ChartFilter/index.stories.js
+++ src/components/HURUmap/ChartFilter/index.stories.js
@@ -12,7 +12,43 @@ const Template = (args) => <ChartFilter {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  helperText: "select a value:",
-  selected: "All Values",
-  options: ["Swahili", "Kikuyu", "Luhya", "All Values"],
+  attributeText: "Filter by attribute:",
+  valueText: "Select a value:",
+  groups: [
+    {
+      subindicators: [
+        "65",
+        "25-35",
+        "15-24",
+        "55-64",
+        "45-54",
+        "36-44",
+        "0-14",
+      ],
+      dataset: 8,
+      name: "age",
+      can_aggregate: true,
+      can_filter: true,
+    },
+    {
+      subindicators: ["Female", "Male"],
+      dataset: 8,
+      name: "gender",
+      can_aggregate: true,
+      can_filter: true,
+    },
+    {
+      subindicators: [
+        "Indian or Asian",
+        "White",
+        "Black African",
+        "Coloured",
+        "Other",
+      ],
+      dataset: 8,
+      name: "race",
+      can_aggregate: true,
+      can_filter: true,
+    },
+  ],
 };
