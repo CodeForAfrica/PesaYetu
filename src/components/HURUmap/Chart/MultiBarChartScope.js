bdacf39ce156f0619bc2d332da3b17f7b915522a
Obed Amoasi <obedamoasi@gmail.com>
2021-10-10 13:47:37 +0000

    [wip] add multi chart scope

---
+++ src/components/HURUmap/Chart/MultiBarChartScope.js
@@ -0,0 +1,334 @@
+import { xAxis, xScale, defaultConfig, commonSignal } from "./properties";
+import { createFiltersForGroups } from "./utils";
+
+import theme from "@/pesayetu/theme";
+
+const PERCENTAGE_TYPE = "percentage";
+const VALUE_TYPE = "value";
+const graphValueTypes = {
+  Percentage: PERCENTAGE_TYPE,
+  Value: VALUE_TYPE,
+};
+
+export default function MultiBarChartScope(
+  primaryData,
+  secondaryData,
+  metadata,
+  config
+) {
+  const {
+    xTicks,
+    defaultType,
+    types: {
+      Value: { formatting: valueFormatting, minX: valueMinX, maxX: valueMaxX },
+      Percentage: {
+        formatting: percentageFormatting,
+        minX: percentageMinX,
+        maxX: percentageMaxX,
+      },
+    },
+  } = config;
+
+  const { primary_group: primaryGroup } = metadata;
+
+  if (xTicks) {
+    xAxis.tickCount = xTicks;
+  }
+
+  const { signals: filterSignals, filters } = createFiltersForGroups(
+    metadata.groups
+  );
+
+  return {
+    $schema: "https://vega.github.io/schema/vega/v5.json",
+    description: "A",
+    config: defaultConfig,
+    autosize: { type: "fit-x", contains: "padding" },
+    padding: 5,
+    width: { signal: "width" },
+    height: { signal: "height" },
+    data: [
+      {
+        name: "primary",
+        values: primaryData,
+        transform: [...filters],
+      },
+      {
+        name: "secondary",
+        values: secondaryData,
+        transform: [...filters],
+      },
+      {
+        name: "primary_data",
+        source: "primary",
+        transform: [
+          {
+            type: "aggregate",
+            ops: ["sum"],
+            as: ["count"],
+            fields: ["count"],
+            groupby: { signal: "groups" },
+          },
+          {
+            type: "joinaggregate",
+            as: ["TotalCount"],
+            ops: ["sum"],
+            fields: ["count"],
+          },
+          {
+            type: "formula",
+            expr: "datum.count/datum.TotalCount",
+            as: "percentage",
+          },
+          {
+            type: "extent",
+            field: "percentage",
+            signal: "percentage_extent",
+          },
+          {
+            type: "extent",
+            field: "count",
+            signal: "value_extent",
+          },
+        ],
+      },
+      {
+        name: "secondary_formatted",
+        source: "secondary",
+        transform: [
+          {
+            type: "aggregate",
+            ops: ["sum"],
+            as: ["count"],
+            fields: ["count"],
+            groupby: { signal: "groups" },
+          },
+          {
+            type: "joinaggregate",
+            as: ["TotalCount"],
+            ops: ["sum"],
+            fields: ["count"],
+          },
+          {
+            type: "formula",
+            expr: "datum.count/datum.TotalCount",
+            as: "percentage",
+          },
+          {
+            type: "extent",
+            field: "percentage",
+            signal: "percentage_extent",
+          },
+          {
+            type: "extent",
+            field: "count",
+            signal: "value_extent",
+          },
+        ],
+      },
+    ],
+    signals: [
+      ...commonSignal,
+      {
+        name: "groups",
+        value: [primaryGroup],
+      },
+      {
+        name: "barvalue",
+        value: "datum",
+      },
+      {
+        name: "Units",
+        value: graphValueTypes[defaultType],
+      },
+      {
+        name: "applyFilter",
+        value: false,
+      },
+      {
+        name: "filterIndicator",
+      },
+      {
+        name: "filterValue",
+      },
+      {
+        name: "mainGroup",
+        value: primaryGroup,
+      },
+      {
+        name: "numberFormat",
+        value: { percentage: percentageFormatting, value: valueFormatting },
+      },
+      {
+        name: "datatype",
+        value: { percentage: "percentage", value: "count" },
+      },
+      {
+        name: "percentageMaxX",
+        value: percentageMaxX !== "default" ? percentageMaxX : undefined,
+      },
+      {
+        name: "percentageMinX",
+        value: percentageMinX !== "default" ? percentageMinX : undefined,
+      },
+      {
+        name: "valueMaxX",
+        value: valueMaxX !== "default" ? valueMaxX : undefined,
+      },
+      {
+        name: "valueMinX",
+        value: valueMinX !== "default" ? valueMinX : undefined,
+      },
+      {
+        name: "domainMin",
+        update: "Units === 'percentage' ? percentageMinX : valueMinX",
+      },
+      {
+        name: "domainMax",
+        update: "Units === 'percentage' ? percentageMaxX : valueMaxX",
+      },
+      {
+        name: "height",
+        update: "bandspace(domain('yscale').length, 0.1, 0.05) * y_step",
+      },
+      ...filterSignals,
+    ],
+    scales: [
+      {
+        name: "yscale",
+        type: "band",
+        domain: { data: "primary_data", field: { signal: "mainGroup" } },
+        range: { step: { signal: "y_step" } },
+        padding: 0.15,
+      },
+      xScale(),
+    ],
+
+    axes: [
+      {
+        orient: "left",
+        scale: "yscale",
+        domainOpacity: 0.5,
+        tickSize: 0,
+        labelPadding: 6,
+        zindex: 1,
+      },
+      xAxis,
+    ],
+
+    marks: [
+      {
+        name: "bars",
+        from: { data: "primary_data" },
+        type: "rect",
+        encode: {
+          enter: {
+            y: { scale: "yscale", field: { signal: "mainGroup" } },
+            height: { scale: "yscale", band: 1 },
+            x: { scale: "xscale", field: { signal: "datatype[Units]" } },
+          },
+          update: {
+            fill: { value: theme.palette.primary.main },
+            x: { scale: "xscale", field: { signal: "datatype[Units]" } },
+            x2: { scale: "xscale", value: 0 },
+            tooltip: {
+              signal:
+                "{'group': datum[mainGroup], 'count': format(datum.count, numberFormat.value)}",
+            },
+          },
+        },
+      },
+      {
+        name: "bars",
+        from: { data: "primary_data" },
+        type: "rect",
+        encode: {
+          enter: {
+            y: { scale: "yscale", field: { signal: "mainGroup" } },
+            height: { scale: "yscale", band: 1 },
+            x: { scale: "xscale", field: { signal: "datatype[Units]" } },
+          },
+          update: {
+            fill: { value: theme.palette.primary.main },
+            x: { scale: "xscale", field: { signal: "datatype[Units]" } },
+            x2: { scale: "xscale", value: 0 },
+            tooltip: {
+              signal:
+                "{'group': datum[mainGroup], 'count': format(datum.count, numberFormat.value)}",
+            },
+          },
+        },
+      },
+      // {
+      //   type: "group",
+      //   name: "primary_bars",
+
+      //   encode: {
+      //     update: {
+      //       x: { value: 0 },
+      //       height: { signal: "height" },
+      //     },
+      //   },
+
+      //   scales: [xScale("primary_data", "width/2")],
+      //   marks: [
+      //     {
+      //       type: "rect",
+      //       from: { data: "primary_data" },
+      //       encode: {
+      //         enter: {
+      //           y: { scale: "yscale", field: { signal: "mainGroup" } },
+      //           height: { scale: "yscale", band: 1 },
+      //           x: { scale: "xscale", field: { signal: "datatype[Units]" } },
+      //         },
+      //         update: {
+      //           fill: { value: theme.palette.primary.main },
+      //           x: { scale: "xscale", field: { signal: "datatype[Units]" } },
+      //           x2: { scale: "xscale", value: 0 },
+      //           tooltip: {
+      //             signal:
+      //               "{'group': datum[mainGroup], 'count': format(datum.count, numberFormat.value)}",
+      //           },
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
+      // {
+      //   type: "group",
+      //   name: "primary_bars",
+
+      //   encode: {
+      //     update: {
+      //       x: { value: "width/2 " },
+      //       height: { signal: "height" },
+      //     },
+      //   },
+
+      //   scales: [xScale("secondary_formatted", "width/2")],
+      //   marks: [
+      //     {
+      //       type: "rect",
+      //       from: { data: "secondary_formatted" },
+      //       encode: {
+      //         enter: {
+      //           y: { scale: "yscale", field: { signal: "mainGroup" } },
+      //           height: { scale: "yscale", band: 1 },
+      //           x: { scale: "xscale", field: { signal: "datatype[Units]" } },
+      //         },
+      //         update: {
+      //           fill: { value: theme.palette.primary.main },
+      //           x: { scale: "xscale", field: { signal: "datatype[Units]" } },
+      //           x2: { scale: "xscale", value: 0 },
+      //           tooltip: {
+      //             signal:
+      //               "{'group': datum[mainGroup], 'count': format(datum.count, numberFormat.value)}",
+      //           },
+      //         },
+      //       },
+      //     },
+      //   ],
+      // },
+    ],
+  };
+}
