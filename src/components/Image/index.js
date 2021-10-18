4921273aff717e1b1a9115f88e56c4124706ee92
Obed Amoasi <obedamoasi@gmail.com>
2021-10-18 10:04:44 +0000

    get dominant color

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,8 +1,31 @@
 import NImage from "next/image";
+import * as Vibrant from "node-vibrant";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
+
+const shimmer = (w, h, color) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <rect width="${w}" height="${h}" fill="${color}" />
+</svg>`;
+
+const toBase64 = (str) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+
+const getDominantColor = async (srcStr) => {
+  let color = "#eee";
+  if (typeof window === "undefined") {
+    color = Vibrant.from(srcStr).getPalette((err, palette) => {
+      return palette.Vibrant.hex;
+    });
+  }
+  return color;
+};
 
 function Image({ src, ...props }) {
+  const [color, setColor] = useState("#eee");
+
   if (!src) {
     return null;
   }
@@ -17,14 +40,18 @@ function Image({ src, ...props }) {
 
   // needed because of this issue https://github.com/vercel/next.js/pull/29367
   if (!srcStr.startsWith("data")) {
-    blurProps.blurDataURL = "data:image/svg+xml;base64,";
+    getDominantColor(srcStr).then((dominantColor) => setColor(dominantColor));
+
+    blurProps.blurDataURL = `data:image/svg+xml;base64,${toBase64(
+      shimmer(1000, 1000, color)
+    )}`;
     blurProps.placeholder = "blur";
   }
   return <NImage {...blurProps} {...props} src={src} />;
 }
 
 Image.propTypes = {
-  src: PropTypes.oneOfType([PropTypes.shape({}), PropTypes.string]),
+  src: PropTypes.string,
 };
 
 Image.defaultProps = {
