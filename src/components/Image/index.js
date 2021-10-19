1b08cc09f5a090ff4350158917decbe81da3f1fe
Obed Amoasi <obedamoasi@gmail.com>
2021-10-19 07:58:26 +0000

    revert placeholder changes

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,57 +1,16 @@
 import NImage from "next/image";
-import * as Vibrant from "node-vibrant";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
-
-const shimmer = (w, h, color) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <rect width="${w}" height="${h}" fill="${color}" />
-</svg>`;
-
-const toBase64 = (str) =>
-  typeof window === "undefined"
-    ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
-
-const getDominantColor = async (srcStr) => {
-  let color = "#eee";
-  if (typeof window === "undefined") {
-    color = Vibrant.from(srcStr).getPalette((err, palette) => {
-      return palette.Vibrant.hex;
-    });
-  }
-  return color;
-};
+import React from "react";
 
 function Image({ src, ...props }) {
-  const [color, setColor] = useState("#eee");
-
   if (!src) {
     return null;
   }
-  const blurProps = {};
-
-  let srcStr = "";
-  if (typeof src === "string") {
-    srcStr = src;
-  } else if (typeof src?.src === "string") {
-    srcStr = src?.src;
-  }
-
-  // needed because of this issue https://github.com/vercel/next.js/pull/29367
-  if (!srcStr.startsWith("data")) {
-    getDominantColor(srcStr).then((dominantColor) => setColor(dominantColor));
-
-    blurProps.blurDataURL = `data:image/svg+xml;base64,${toBase64(
-      shimmer(1000, 1000, color)
-    )}`;
-    blurProps.placeholder = "blur";
-  }
-  return <NImage {...blurProps} {...props} src={src} />;
+  return <NImage {...props} src={src} />;
 }
 
 Image.propTypes = {
-  src: PropTypes.string,
+  src: PropTypes.oneOfType([PropTypes.shape({}), PropTypes.string]),
 };
 
 Image.defaultProps = {
