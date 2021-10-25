4db463c2580b8f718f6ea74e97631eb04fac610b
_ Kilemensi <clemence@codeforafrica.org>
2021-10-25 14:25:03 +0300

    Enable blur if blurDataURL included in src

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -2,18 +2,22 @@ import NImage from "next/image";
 import PropTypes from "prop-types";
 import React from "react";
 
-function Image({ src, ...props }) {
+function Image({ placeholder: placeholderProp, src, ...props }) {
   if (!src) {
     return null;
   }
-  return <NImage {...props} src={src} />;
+  const hasBlurData = !!src?.blurDataURL;
+  const placeholder = placeholderProp || hasBlurData ? "blur" : undefined;
+  return <NImage {...props} placeholder={placeholder} src={src} />;
 }
 
 Image.propTypes = {
+  placeholder: PropTypes.string,
   src: PropTypes.oneOfType([PropTypes.shape({}), PropTypes.string]),
 };
 
 Image.defaultProps = {
+  placeholder: undefined,
   src: undefined,
 };
 
