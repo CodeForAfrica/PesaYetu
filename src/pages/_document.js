57bb2e0ddf528b0019c879bcaaafee94bae76f67
Obed Amoasi <obedamoasi@gmail.com>
2021-11-01 07:43:10 +0000

    add analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,23 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           {/* Used in page headers */}
           <link
             rel="preload"
