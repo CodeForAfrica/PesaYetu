55d8d49cd9ff05dd04315aa026377f8d8adf4bfb
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-02-28 13:27:16 +0300

    Fix styles

--- src/components/CopyToClipBoard/useStyles.js
+++ src/components/CopyToClipBoard/useStyles.js
@@ -1,21 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(({ palette, typography }) => ({
-  root: {
-    display: "flex",
-  },
-  children: {
-    display: "inline-flex",
-    alignItems: "center",
-    marginRight: typography.pxToRem(20),
-    fontSize: typography.pxToRem(16),
-  },
-  tooltip: {
-    marginTop: 0,
-    backgroundColor: palette.common.white,
-    color: "rgba(0, 0, 0, 0.87)",
-    fontSize: 11,
-  },
+const useStyles = makeStyles(({ typography }) => ({
   icon: {
     marginLeft: typography.pxToRem(1),
   },
