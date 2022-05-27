46b9716014e6fadc163ac1d84a5a43b4d69df5d0
_ Kilemensi <clemence@codeforafrica.org>
2022-05-27 16:14:56 +0300

    Handle multiplier when method is percentage

--- src/utils/formatNumericalValue.js
+++ src/utils/formatNumericalValue.js
@@ -5,13 +5,18 @@ import { hurumap } from "@/pesayetu/config";
 function formatNumericalValue({ value, method }) {
   const { formatting } = hurumap;
   let fn = format(formatting.percentage);
+  // Percentage formatting multiplies by 100 first.
+  // see: https://github.com/d3/d3-format#locale_format
+  let multipler = 100;
   if (method === "absolute_value") {
     fn = format(formatting.integer);
+    multipler = 1;
   } else if (method === "decimal") {
+    multipler = 1;
     fn = format(formatting.decimal);
   }
 
-  return fn(value);
+  return fn(value / multipler);
 }
 
 export default formatNumericalValue;
