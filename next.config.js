b34b27bdeeaf530c74f162bdc432877c85fd1aae
Kipruto <43873157+kelvinkipruto@users.noreply.github.com>
2023-07-20 10:41:33 +0300

    Upgrade packages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
+const path = require("path");
+
+const PROJECT_ROOT = process.env.PROJECT_ROOT?.trim();
+const outputFileTracingRoot = PROJECT_ROOT
+  ? path.resolve(__dirname, PROJECT_ROOT)
+  : undefined;
+
 module.exports = {
   experimental: {
-    outputStandalone: true,
+    outputFileTracingRoot,
   },
   images: {
     domains: process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(",")
       ?.map((d) => d.trim())
       ?.filter((d) => d),
+    unoptimized:
+      process.env.NEXT_PUBLIC_IMAGE_UNOPTIMIZED?.trim()?.toLowerCase() ===
+      "true",
   },
+  modularizeImports: {
+    // NOTE: only transform @mui/material and not any of sub-modules e.g. @mui/material/styles.
+    "@mui/material^": {
+      transform: "@mui/material/{{member}}",
+    },
+  },
+  output: "standalone",
   reactStrictMode: false,
   webpack(config) {
     config.module.rules.push({
